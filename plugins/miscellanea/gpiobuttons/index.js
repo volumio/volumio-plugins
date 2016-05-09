d688e8293a93741a01a87676db5dc355a6d6a424
balbuze <balbuze@gmail.com>
2016-05-09 18:38:49 +0200

    small work on gpiobuttons plugin

--- plugins/miscellanea/gpiobuttons/index.js
+++ plugins/miscellanea/gpiobuttons/index.js
@@ -19,7 +19,37 @@ function Gpiobuttons(context) {
 
 Gpiobuttons.prototype.onVolumioStart = function () {
 	var self = this;
-	
+//have a look here : https://learn.adafruit.com/node-embedded-development?view=all
+self.libSocketIO.on('connection', function (connWebSocket) {
+  connWebSocket.on('bringmepizza', function () {
+  givehimpizza();
+});
+});
+
+// button is attaced to pin 17, led to 18
+var GPIO = require('onoff').Gpio,
+ //   led = new GPIO(18, 'out'),
+	cmdmpd =    
+	 button = new GPIO(17, 'in', 'both');
+
+// define the callback function
+function light(err, state) {
+  
+  // check the state of the button
+  // 1 == pressed, 0 == not pressed
+  if(state == 1) {
+    // turn LED on
+    led.writeSync(1);
+  } else {
+    // turn LED off
+    led.writeSync(0);
+  }
+  
+}
+
+// pass the callback function to the
+// as the first argument to watch()
+button.watch(light);	
 };
 
 Gpiobuttons.prototype.onStart = function () {
