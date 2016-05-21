5def7073bb0eb665da5fbc9b966c2898bab1bf4d
balbuze <balbuze@gmail.com>
2016-05-21 10:52:51 +0200

    work on unmutedigiamp - rewrite with wiring pi lib - instalable plugin - activation ok - but volumio does not restart

--- plugins/miscellanea/unmutedigiamp/install.sh
+++ plugins/miscellanea/unmutedigiamp/install.sh
@@ -3,21 +3,7 @@
 echo "Installing "
 echo "Installing unmutedigiamp"
 echo "Installing WiringPi"
-wget http://repo.volumio.org/Volumio2/Binaries/wiringpi_2.24_armhf.deb
-sudo dpkg -i wiringpi_2.24_armhf.deb
-rm /wiringpi_2.24_armhf.deb
 
-echo "adding gpio group and permissions"
-cd /
-wget http://repo.volumio.org/Volumio2/Binaries/gpio-admin.tar.gz
-sudo tar xvf gpio-admin.tar.gz
-sudo rm /gpio-admin.tar.gz
-sudo groupadd -f --system gpio
-sudo chgrp gpio /usr/local/bin/gpio-admin
-sudo chmod u=rwxs,g=rx,o= /usr/local/bin/gpio-admin
-
-echo "adding volumio to gpio group"
-sudo adduser volumio gpio 
 
 #requred to end the plugin install
 echo "plugininstallend"
