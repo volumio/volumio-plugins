623f6359f9c782fcd17e93290de0256743d70663
Massimiliano Fanciulli <fanciulli@gmail.com>
2016-06-07 19:41:20 +0200

    Implemented Genre browsing

--- plugins/music_service/shoutcast/index.js
+++ plugins/music_service/shoutcast/index.js
@@ -14,6 +14,7 @@ function ControllerShoutcast(context) {
 	this.logger = this.context.logger;
 	this.configManager = this.context.configManager;
 
+    
 }
 
 
@@ -48,27 +49,22 @@ ControllerShoutcast.prototype.handleBrowseUri=function(curUri)
     var self=this;
 
     var response;
+
+    self.logger.info("SHOUCAST BROWSE URI: "+curUri);
     if (curUri.startsWith('shoutcast')) {
         if (curUri == 'shoutcast')
             response = self.listRoot(curUri);
         else {
-            if (curUri.startsWith('radio/myWebRadio')) {
+            if (curUri.startsWith('shoutcast/myWebRadio')) {
                 response = self.listMyWebRadio(curUri);
             }
-            else if (curUri.startsWith('radio/favourites'))
+            else if (curUri.startsWith('shoutcast/favourites'))
                 response = self.listRadioFavourites(curUri);
-            else if (curUri.startsWith('radio/byGenre')) {
-                if (curUri == 'radio/byGenre')
-                    response = self.listRadioCategories(curUri);
+            else if (curUri.startsWith('shoutcast/byGenre')) {
+                if (curUri == 'shoutcast/byGenre')
+                    response = self.listRadioGenres(curUri);
                 else
-                    response = self.listRadioForCategory(curUri);
-
-            }
-            else if (curUri.startsWith('radio/byCountry')) {
-                if (curUri == 'radio/byCountry')
-                    response = self.listRadioCountries(curUri);
-                else
-                    response = self.listRadioForCountry(curUri);
+                    response = self.listRadioForGenres(curUri);
 
             }
         }
@@ -111,18 +107,115 @@ ControllerShoutcast.prototype.listRoot=function()
                     album: '',
                     icon: 'fa fa-tags',
                     uri: 'shoutcast/byGenre'
-                },
-                {
-                    service: 'shoutcast',
-                    type: 'radio-category',
-                    title: 'By Country',
-                    artist: '',
-                    album: '',
-                    icon: 'fa fa-globe',
-                    uri: 'shoutcast/byCountry'
                 }
 
             ]
         }
     });
 }
+
+ControllerShoutcast.prototype.listRadioGenres = function () {
+    var self = this;
+
+    var defer = libQ.defer();
+
+    var response = {
+        navigation: {
+            prev: {
+                uri: 'shoutcast'
+            },
+            list: []
+        }
+    };
+
+    unirest.get('http://api.shoutcast.com/legacy/genrelist?k=vKgHQrwysboWzMwH')
+    .end(function (xml) {
+        if(xml.ok)
+        {
+            var xmlDoc = libxmljs.parseXml(xml.body);
+
+            var children = xmlDoc.root().childNodes();
+
+            for(var i in children)
+            {
+                var name=children[i].attr('name').value();
+                var category = {
+                    type: 'radio-category',
+                    title: name,
+                    icon: 'fa fa-folder-open-o',
+                    uri: 'shoutcast/byGenre/' + name
+                };
+
+                response.navigation.list.push(category);
+            }
+
+            defer.resolve(response);
+        }
+        else defer.reject(new Error('An error occurred while querying SHOUTCAST'));
+    });
+
+
+    return defer.promise;
+};
+
+ControllerShoutcast.prototype.listRadioForGenres = function (curUri) {
+    var self = this;
+
+    var defer = libQ.defer();
+
+    var genre=curUri.split('/')[2];
+    self.logger.info("GENRE: "+genre);
+
+
+    var response = {
+        navigation: {
+            prev: {
+                uri: 'shoutcast/byGenre'
+            },
+            list: []
+        }
+    };
+
+    unirest.get('http://api.shoutcast.com/legacy/genresearch?k=vKgHQrwysboWzMwH&genre='+genre)
+        .end(function (xml) {
+            if(xml.ok)
+            {
+                var xmlDoc = libxmljs.parseXml(xml.body);
+
+                var children = xmlDoc.root().childNodes();
+                var base;
+
+                for(var i in children)
+                {
+                    if(children[i].name()==='tunein')
+                    {
+                        base=children[i].attr('base').value();
+                    }
+                    else if(children[i].name()==='station')
+                    {
+                        var name=children[i].attr('name').value();
+                        var id=children[i].attr('id').value();
+
+                        var category = {
+                            service: 'shoutcast',
+                            type: 'webradio',
+                            title: name,
+                            artist: '',
+                            album: '',
+                            icon: 'fa fa-microphone',
+                            uri: 'http://yp.shoutcast.com' + base+'?id='+id
+                        };
+
+                        response.navigation.list.push(category);
+                    }
+
+                }
+
+                defer.resolve(response);
+            }
+            else defer.reject(new Error('An error occurred while querying SHOUTCAST'));
+        });
+
+
+    return defer.promise;
+};
