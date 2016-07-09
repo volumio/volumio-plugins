cae1c508c16f8daa5eef12ea418fbaa3e9ac3a39
balbuze <balbuze@gmail.com>
2016-07-09 12:06:16 +0200

    first basic spotify connect for volumio -add avahi support

--- plugins/music_service/volspotconnect/install.sh
+++ plugins/music_service/volspotconnect/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-
+echo "Installing spotify-connect-web dependencies"
+sudo apt-get update
+sudo apt-get -y install avahi-utils
 #required to end the plugin install
 echo "plugininstallend"
