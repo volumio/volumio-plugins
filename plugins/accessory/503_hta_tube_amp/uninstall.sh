ad94d644b0f005166256385c8eb78e82fa69304d
balbuze <balbuze@users.noreply.github.com>
2016-11-26 11:35:09 +0100

    Update uninstall.sh
    correct plugin reference

--- plugins/accessory/503_hta_tube_amp/uninstall.sh
+++ plugins/accessory/503_hta_tube_amp/uninstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "Unistalling gpiobuttons dependencies"
+echo "Uninstalling 503_hta_tube_amp dependencies"
 
-echo "Removing Touch display"
+echo "Removing 503_hta_tube_amp"
 
 echo "Done"
