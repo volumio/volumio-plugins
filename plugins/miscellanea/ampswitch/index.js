d43eafb24bfcec2df8bb9fb71f70f01522d067ce
Tim Lange <tim.lange@cs.rwth-aachen.de>
2017-01-14 00:13:23 +0100

    First functional version of amplifier switch

--- plugins/miscellanea/ampswitch/index.js
+++ plugins/miscellanea/ampswitch/index.js
@@ -1,12 +1,14 @@
 'use strict';
 
-var exec = require('child_process').exec;
-var os = require('os');
+// load external modules
 var libQ = require('kew');
 var io = require('socket.io-client');
+var Gpio = require('onoff').Gpio;
+
 var socket = io.connect('http://localhost:3000');
-var Gpio = require('onoff').Gpio,
-shutdown = new Gpio(14, 'high');
+
+
+//declare global status variable
 var status = 'stop';
 
 // Define the AmpSwitchController class
@@ -15,104 +17,143 @@ module.exports = AmpSwitchController;
 
 function AmpSwitchController(context) {
 	var self = this;
+
 	// Save a reference to the parent commandRouter
 	self.context = context;
 	self.commandRouter = self.context.coreCommand;
 	self.logger=self.commandRouter.logger;
 	this.configManager = this.context.configManager;
+
+    //define timer and shutdown variables
     self.timer;
+    self.shutdown;
 
 }
 
+// define behaviour on system start up. In our case just read config file
 AmpSwitchController.prototype.onVolumioStart = function()
 {
-	var self = this;
-    self.logger.info("AmpSwitch loaded");
+    var self = this;
+    var configFile=this.commandRouter.pluginManager.getConfigurationFile(this.context,'config.json');
+    this.config = new (require('v-conf'))();
+    this.config.loadFile(configFile);
 
 }
 
+// Volumio needs this
 AmpSwitchController.prototype.getConfigurationFiles = function()
 {
 	return ['config.json'];
 }
 
+// define behaviour on plugin activation
 AmpSwitchController.prototype.onStart = function() {
 	var self = this;
-
 	var defer=libQ.defer();
 
-    socket.emit('getState','');
-
-    socket.once('pushState', function (state) {
-        status=state.status;
-    });
+    // initialize output port
     self.ampGPIOInit();
-    self.logger.info('AmpSwitch started');
-    defer.resolve();
 
+    // read and parse status once
+    socket.emit('getState','');
+    socket.once('pushState', self.parseStatus.bind(self));
+
+    // listen to every subsequent status report from Volumio
+    // status is pushed after every playback action, so we will be
+    // notified if the status changes
+    socket.on('pushState', self.parseStatus.bind(self));
+
+    defer.resolve();
 	return defer.promise;
 };
 
+// define behaviour on plugin deactivation.
 AmpSwitchController.prototype.onStop = function() {
     var self = this;
     var defer=libQ.defer();
 
+    // we don't have to claim GPIOs any more
     self.freeGPIO();
-    clearInterval(self.timer);
+
     return defer.promise;
 };
 
+// initialize Plugin settings page
 AmpSwitchController.prototype.getUIConfig = function() {
-	var defer = libQ.defer();
-	var self = this;
+    var defer = libQ.defer();
+    var self = this;
 
-	return defer.promise;
+    var lang_code = this.commandRouter.sharedVars.get('language_code');
+
+    self.commandRouter.i18nJson(__dirname+'/i18n/strings_'+lang_code+'.json',
+                                __dirname+'/i18n/strings_en.json',
+                                __dirname + '/UIConfig.json')
+    .then(function(uiconf)
+          {
+          uiconf.sections[0].content[0].value = self.config.get('port');
+          defer.resolve(uiconf);
+          })
+    .fail(function()
+          {
+          defer.reject(new Error());
+          });
+    
+    return defer.promise;
 };
 
+// define what happens when the user clicks the 'save' button on the settings page
+AmpSwitchController.prototype.saveOptions = function(data) {
+    var self = this;
+
+    // save port setting to our config
+    self.config.set('port', data['port_setting']);
+    // unexport GPIOs before constructing new GPIO object
+    self.freeGPIO();
+    self.ampGPIOInit();
+
+    // output message about successful saving to the UI
+    self.commandRouter.pushToastMessage('success','Output port', 'saved');
+};
+
+// initialize shutdown port to the one that we stored in the config
 AmpSwitchController.prototype.ampGPIOInit = function() {
     var self = this;
 
-    self.timer = setInterval(self.pollStatus.bind(self),2000);
-    if(status=='play') {
-        self.on();
-    } else if(status=='stop' || status=='pause') {
-        self.off();
-    }
-    self.logger.info('AmpSwitch initialized');
+    self.shutdown = new Gpio(self.config.get('port')['value'],'out');
 };
 
-AmpSwitchController.prototype.pollStatus = function() {
+// a pushState event has happened. Check whether it differs from the last known status and
+// switch output port on or off respectively
+AmpSwitchController.prototype.parseStatus = function(state) {
     var self = this;
 
-    self.logger.info('Polling status (' + status + ')');
-    socket.emit('getState','');
+    if(state.status=='play' && state.status!=status){
+        status=state.status;
+        self.on();
+    } else if((state.status=='pause' || state.status=='stop') && (status!='pause' && status!='stop')){
+        status=state.status;
+        self.off();
+    }
 
-    socket.once('pushState', function (state) {
-        if(state.status=='play' && state.status != status){
-            status=state.status;
-            self.on();
-        } else if((state.status=='pause' || state.status=='stop') && (status!='pause' && status!='stop')){
-            status=state.status;
-            self.off();
-        }
-    });
 };
 
+// switch outport port on
 AmpSwitchController.prototype.on = function() {
+    var self = this;
 
-    self.logger.info('AmpSwitch: Output ON');
-    shutdown.writeSync(1);
+    self.shutdown.writeSync(1);
 };
 
-
+//switch output port off
 AmpSwitchController.prototype.off = function() {
+    var self = this;
 
-    self.logger.info('AmpSwitch: Output OFF');
-    shutdown.writeSync(0);
+    self.shutdown.writeSync(0);
 };
 
+// stop claiming output port
 AmpSwitchController.prototype.freeGPIO = function() {
     var self = this;
 
-    shutdown.unexport();
+    self.shutdown.unexport();
 };
--- plugins/miscellanea/ampswitch/install.sh
+++ plugins/miscellanea/ampswitch/install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+mkdir /data/configuration/miscellanea/ampswitch
+touch /data/configuration/miscellanea/ampswitch/config.json
+
 #requred to end the plugin install
 echo "plugininstallend"
