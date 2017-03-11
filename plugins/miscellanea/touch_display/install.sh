85124560843601618b92a31be3765399ef4e4aa4
volumio <info@volumio.org>
2017-03-11 03:49:07 +0000

    touchscreen install fix

--- plugins/miscellanea/touch_display/install.sh
+++ plugins/miscellanea/touch_display/install.sh
@@ -1,23 +1,61 @@
 #!/bin/bash
 
-echo "Installing Chromium Dependencies"
-sudo apt-get update
-sudo apt-get -y install
+HW=$(awk '/VOLUMIO_HARDWARE=/' /etc/*-release | sed 's/VOLUMIO_HARDWARE=//' | sed 's/\"//g')
 
-echo "Installing Graphical environment"
-sudo apt-get install -y xinit xorg openbox libexif12
+if [ "$HW" = "pi" ];
+then
+  echo "Raspberry Pi install script"
 
-echo "Download Chromium"
-cd /home/volumio/
-wget http://launchpadlibrarian.net/234969703/chromium-browser_48.0.2564.82-0ubuntu0.15.04.1.1193_armhf.deb
-wget http://launchpadlibrarian.net/234969705/chromium-codecs-ffmpeg-extra_48.0.2564.82-0ubuntu0.15.04.1.1193_armhf.deb
+  echo "Installing fake packages for kernel, bootloader and pi lib"
+  wget http://repo.volumio.org/Volumio2/Binaries/arm/libraspberrypi0_0.0.1_all.deb
+  wget http://repo.volumio.org/Volumio2/Binaries/arm/raspberrypi-bootloader_0.0.1_all.deb
+  wget http://repo.volumio.org/Volumio2/Binaries/arm/raspberrypi-kernel_0.0.1_all.deb
+  dpkg -i libraspberrypi0_0.0.1_all.deb
+  dpkg -i raspberrypi-bootloader_0.0.1_all.deb
+  dpkg -i raspberrypi-kernel_0.0.1_all.deb
+  rm libraspberrypi0_0.0.1_all.deb
+  rm raspberrypi-bootloader_0.0.1_all.deb
+  rm raspberrypi-kernel_0.0.1_all.deb
 
-echo "Install  Chromium"
-sudo dpkg -i /home/volumio/chromium-*.deb
-sudo apt-get install -y -f
-sudo dpkg -i /home/volumio/chromium-*.deb
+  echo "Putting on hold packages for kernel, bootloader and pi lib"
+  echo "libraspberrypi0 hold" | dpkg --set-selections
+  echo "raspberrypi-bootloader hold" | dpkg --set-selections
+  echo "raspberrypi-kernel hold" | dpkg --set-selections
 
-rm /home/volumio/chromium-*.deb
+
+  echo "Installing Chromium Dependencies"
+  sudo apt-get update
+  sudo apt-get -y install
+
+  echo "Installing Graphical environment"
+  sudo apt-get install -y xinit xorg openbox libexif12
+
+  echo "Installing Chromium"
+  sudo apt-get install -y chromium-browser
+else
+
+  echo "Installing Chromium Dependencies"
+  sudo apt-get update
+  sudo apt-get -y install
+
+  echo "Installing Graphical environment"
+  sudo apt-get install -y xinit xorg openbox libexif12
+
+  echo "Download Chromium"
+  cd /home/volumio/
+  wget http://launchpadlibrarian.net/234969703/chromium-browser_48.0.2564.82-0ubuntu0.15.04.1.1193_armhf.deb
+  wget http://launchpadlibrarian.net/234969705/chromium-codecs-ffmpeg-extra_48.0.2564.82-0ubuntu0.15.04.1.1193_armhf.deb
+
+  echo "Install  Chromium"
+  sudo dpkg -i /home/volumio/chromium-*.deb
+  sudo apt-get install -y -f
+  sudo dpkg -i /home/volumio/chromium-*.deb
+
+  rm /home/volumio/chromium-*.deb
+
+fi
+
+echo "Dependencies installed"
 
 echo "  Creating chromium kiosk start script"
 echo "#!/bin/bash
@@ -36,6 +74,7 @@ while true; do
     --disable-infobars \\
     --disable-session-crashed-bubble \\
     --disable-translate \\
+	--no-sandbox \
     http://localhost:3000
 done" > /opt/volumiokiosk.sh
 /bin/chmod +x /opt/volumiokiosk.sh
@@ -47,8 +86,8 @@ Wants=volumio.service
 After=volumio.service
 [Service]
 Type=simple
-User=volumio
-Group=audio
+User=root
+Group=root
 ExecStart=/usr/bin/startx /etc/X11/Xsession /opt/volumiokiosk.sh
 # Give a reasonable amount of time for the server to start up/shut down
 TimeoutSec=300
