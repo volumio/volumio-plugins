5ae7cbc6460f8cb056cebd9ad96fccf2765f04b3
volumio <volumio@localhost>
2017-05-08 21:12:30 +0000

    do not install libasound dev and do ldconfig on install

--- plugins/music_service/spotify/install.sh
+++ plugins/music_service/spotify/install.sh
@@ -2,7 +2,7 @@
 
 echo "Installing Spop Dependencies"
 sudo apt-get update
-sudo apt-get -y install libao-dev libglib2.0-dev libjson-glib-1.0-0 libjson-glib-dev libao-common libasound2-dev libreadline-dev libsox-dev libsoup2.4-dev libsoup2.4-1 libdbus-glib-1-dev libnotify-dev --no-install-recommends
+sudo apt-get -y install libao-dev libglib2.0-dev libjson-glib-1.0-0 libjson-glib-dev libao-common libreadline-dev libsox-dev libsoup2.4-dev libsoup2.4-1 libdbus-glib-1-dev libnotify-dev --no-install-recommends
 
 
 
@@ -16,7 +16,7 @@ wget http://repo.volumio.org/Packages/Spop/spop-${DPKG_ARCH}.tar.gz
 sudo tar xvf /tmp/spop-${DPKG_ARCH}.tar.gz -C /
 rm /tmp/spop-${DPKG_ARCH}.tar.gz
 
-
+ldconfig
 sudo chmod 777 /etc/spopd.conf
 
 #requred to end the plugin install
