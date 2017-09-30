d4789f057e0c80af46a61a6b5c0439c71d2de975
ashthespy <ashthespy@gmail.com>
2017-09-30 19:30:02 +0200

    Add metadata and state support
    Read metadata and state information from modified spotify-connect-web
    (SpotConnect) which runs exposes a socket interface

---
+++ plugins/music_service/volspotconnect/SpotConnController.js
@@ -0,0 +1,57 @@
+const dgram = require('dgram');
+const EventEmitter = require('events');
+
+class SpotConnEvents extends EventEmitter {
+  constructor(opts) {
+    super()
+    var self = this;
+    self._udpsource = dgram.createSocket('udp4');
+    self._udpsource.bind(opts.port)
+
+    self._udpsource.on('error', function(err) {
+      console.log(err);
+    });
+
+    self._udpsource.on('message', msg => {
+
+      this.parseData(msg.toString())
+    })
+  }
+
+  parseData(data) {
+    // Quick and dirty JSON check
+    try {
+      const metadata = JSON.parse(data);
+      this.emit('metadata', metadata);
+    } catch (e) {
+      switch (data) {
+        case 'kSpPlaybackNotifyBecameActive':
+          this.emit('SActive', '')
+          break;
+
+        case 'kSpDeviveActive':
+          this.emit('DActive', '')
+          break;
+
+        case 'kSpDeviveInactive':
+          this.emit('DInactive', '')
+          break;
+
+        case 'SInactive':
+          this.emit('kSpPlaybackNotifyBecameInactive')
+          break;
+
+        default:
+          this.emit('unknown', data)
+      };
+    }
+  }
+
+  sendmsg(msg) {
+    // Attempting to send a message back via udp
+    // This is WIP and will do nothing now
+    self._udpsource.send(msg)
+  }
+}
+
+module.exports = SpotConnEvents
