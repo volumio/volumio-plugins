a9431ca99cb0059b7cf1facbb1d868397c60a2b5
ashthespy <ashthespy@gmail.com>
2018-02-04 13:41:22 +0100

    Add preliminary metadata support with modified librespot

---
+++ plugins/music_service/volspotconnect2/SpotConnController.js
@@ -0,0 +1,61 @@
+const dgram = require('dgram');
+const EventEmitter = require('events');
+
+class SpotConnEvents extends EventEmitter {
+  constructor(opts) {
+    super()
+    var self = this;
+    self._udpsource = dgram.createSocket('udp4');
+    self._udpsource.bind(opts.port)
+    self._udpsource.on('error', function(err) {
+      console.log(err);
+    });
+
+    self._udpsource.on('message', msg => {
+
+      this.parseData(msg.toString())
+    })
+  }
+
+  parseData(data) {
+    // Quick and dirty JSON check
+    console.log(data);
+    try {
+      const metadata = JSON.parse(data);
+      this.emit('metadata', metadata);
+    } catch (e) {
+      switch (data) {
+        case 'kSpPlaybackNotifyBecameActive':
+          this.emit('SActive', '')
+          break;
+
+        case 'kSpDeviveActive':
+          this.emit('DActive', '')
+          break;
+
+        case 'kSpDeviveInactive':
+          this.emit('DInactive', '')
+          break;
+
+        case 'kSpPlaybackNotifyBecameInactive':
+          this.emit('SInactive')
+          break;
+
+        default:
+          this.emit('unknown', data)
+      };
+    }
+  }
+
+  sendmsg(msg){
+    // Attempting to send a message back via udp
+    self._udpsource.send(msg)
+
+  }
+}
+
+
+
+
+
+module.exports = SpotConnEvents
