298f380c80bbe3b6010ecf1ae9ac88becfa5be89
Rachid Groeneveld <Saiyato@users.noreply.github.com>
2018-02-19 08:39:00 +0100

    Extended uninstall
    Uninstalling now removes all unnecessary files.

--- plugins/miscellanea/kodi/uninstall.sh
+++ plugins/miscellanea/kodi/uninstall.sh
@@ -2,6 +2,7 @@
 echo "Uninstalling Kodi configs"
 
 # Remove policies
+echo "Removing policies..."
 rm /etc/udev/rules.d/99-input.rules
 rm /etc/udev/rules.d/10-permissions.rules
 rm /etc/ld.so.conf.d/00-vmcs.conf
@@ -9,15 +10,23 @@ rm /etc/polkit-1/localauthority/50-local.d/50-kodi-actions.pkla
 ldconfig
 
 # Update the GPU memory
+echo "Update the GPU memory..."
 CONFIG="/boot/config.txt"
 echo "Updating GPU memory back to 16MB..."
 sed -i -- 's|.*gpu_mem.*|gpu_mem=16|g' $CONFIG
 
 # Remove the systemd unit
+echo "Removing the systemd unit..."
 rm /etc/systemd/system/kodi.service
 systemctl daemon-reload
 
+echo "Uninstalling packages and purging..."
 apt-get purge --auto-remove gdb kodi --yes
+
+# Own the files before deleting them
+echo "Cleaning up directories..."
+chown -R volumio /data/configuration/miscellanea/kodi/
+rm -rf /data/configuration/miscellanea/kodi/
 userdel -r kodi
 
 # The end...
