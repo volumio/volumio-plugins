b0c48e28f4729325956a4c913a295847badc72bd
Ben Mitchell <ben@orderoftheflame.co.uk>
2018-03-07 21:56:13 +0000

    Prettify js.

--- plugins/miscellanea/onkyo_control/index.js
+++ plugins/miscellanea/onkyo_control/index.js
@@ -1,7 +1,6 @@
-
 var libQ = require('kew');
-var fs=require('fs-extra');
-var config = new (require('v-conf'))();
+var fs = require('fs-extra');
+var config = new(require('v-conf'))();
 var exec = require('child_process').exec;
 var execSync = require('child_process').execSync;
 
@@ -12,72 +11,62 @@ var eiscp = require('eiscp');
 var currentState;
 
 module.exports = onkyoControl;
-function onkyoControl(context) {
-	var self = this;
 
-	this.context = context;
-	this.commandRouter = this.context.coreCommand;
-	this.logger = this.context.logger;
-	this.configManager = this.context.configManager;
+function onkyoControl(context) {
+    var self = this;
+
+    this.context = context;
+    this.commandRouter = this.context.coreCommand;
+    this.logger = this.context.logger;
+    this.configManager = this.context.configManager;
 
 }
 
-
-
-onkyoControl.prototype.onVolumioStart = function()
-{
-	var self = this;
-	var configFile=this.commandRouter.pluginManager.getConfigurationFile(this.context,'config.json');
-	this.config = new (require('v-conf'))();
-	this.config.loadFile(configFile);
+onkyoControl.prototype.onVolumioStart = function() {
+    var self = this;
+    var configFile = this.commandRouter.pluginManager.getConfigurationFile(this.context, 'config.json');
+    this.config = new(require('v-conf'))();
+    this.config.loadFile(configFile);
 
     return libQ.resolve();
 }
 
 onkyoControl.prototype.onStart = function() {
     var self = this;
-    var defer=libQ.defer();
+    var defer = libQ.defer();
 
 
     // Once the Plugin has successfull started resolve the promise
     defer.resolve();
 
 
-     eiscp.on('connect', function () {
-	if (self.currentState === 'play') {
-		self.logger.info("ONKYO-CONTROL:  eiscp.command('system-power=on')");
-	        eiscp.command('system-power=on', function () {
-			self.logger.info("ONKYO-CONTROL:  eiscp.disconnect()");
-                        eiscp.disconnect();
-			 self.logger.info("ONKYO-CONTROL:  eiscp.disconnect() finished");
-                });
-      	} else if (self.currentState === 'stop') {
-                self.logger.info("ONKYO-CONTROL:  eiscp.command('system-power=standby')");
-                eiscp.command('system-power=standby', function () {
-                        self.logger.info("ONKYO-CONTROL:  eiscp.disconnect()");
-                        eiscp.disconnect();
-                         self.logger.info("ONKYO-CONTROL:  eiscp.disconnect() finished");
-                });
-	}
+    eiscp.on('connect', function() {
+        if (self.currentState === 'play') {
+            self.logger.info("ONKYO-CONTROL:  eiscp.command('system-power=on')");
+            eiscp.command('system-power=on', function() {
+                self.logger.info("ONKYO-CONTROL:  eiscp.disconnect()");
+                eiscp.disconnect();
+                self.logger.info("ONKYO-CONTROL:  eiscp.disconnect() finished");
+            });
+        } else if (self.currentState === 'stop') {
+            self.logger.info("ONKYO-CONTROL:  eiscp.command('system-power=standby')");
+            eiscp.command('system-power=standby', function() {
+                self.logger.info("ONKYO-CONTROL:  eiscp.disconnect()");
+                eiscp.disconnect();
+                self.logger.info("ONKYO-CONTROL:  eiscp.disconnect() finished");
+            });
+        }
     });
 
+    socket.on('pushState', function(state) {
 
-
-
-
-
-
-
-
-    socket.on('pushState', function (state) {
-
-	if (self.currentState && state.status !== self.currentState && !eiscp.is_connected) {
-		self.logger.info("ONKYO-CONTROL: *********** ONKYO PLUGIN STATE CHANGE ********");
-		self.logger.info("ONKYO-CONTROL: New state: " + JSON.stringify(state));
-		self.logger.info("ONKYO-CONTROL: eiscp not connected. Connecting... ");
-		eiscp.connect({ port: 60128, reconnect: false, reconnect_sleep: 5, modelsets: [], send_delay: 500, verify_commands: false });
-	}
-	self.currentState = state.status;
+        if (self.currentState && state.status !== self.currentState && !eiscp.is_connected) {
+            self.logger.info("ONKYO-CONTROL: *********** ONKYO PLUGIN STATE CHANGE ********");
+            self.logger.info("ONKYO-CONTROL: New state: " + JSON.stringify(state));
+            self.logger.info("ONKYO-CONTROL: eiscp not connected. Connecting... ");
+            eiscp.connect({ port: 60128, reconnect: false, reconnect_sleep: 5, modelsets: [], send_delay: 500, verify_commands: false });
+        }
+        self.currentState = state.status;
 
     });
 
@@ -88,7 +77,7 @@ onkyoControl.prototype.onStart = function() {
 
 onkyoControl.prototype.onStop = function() {
     var self = this;
-    var defer=libQ.defer();
+    var defer = libQ.defer();
 
     // Once the Plugin has successfull stopped resolve the promise
     defer.resolve();
@@ -110,17 +99,15 @@ onkyoControl.prototype.getUIConfig = function() {
 
     var lang_code = this.commandRouter.sharedVars.get('language_code');
 
-    self.commandRouter.i18nJson(__dirname+'/i18n/strings_'+lang_code+'.json',
-        __dirname+'/i18n/strings_en.json',
-        __dirname + '/UIConfig.json')
-        .then(function(uiconf)
-        {
+    self.commandRouter.i18nJson(__dirname + '/i18n/strings_' + lang_code + '.json',
+            __dirname + '/i18n/strings_en.json',
+            __dirname + '/UIConfig.json')
+        .then(function(uiconf) {
 
 
             defer.resolve(uiconf);
         })
-        .fail(function()
-        {
+        .fail(function() {
             defer.reject(new Error());
         });
 
@@ -129,16 +116,16 @@ onkyoControl.prototype.getUIConfig = function() {
 
 
 onkyoControl.prototype.setUIConfig = function(data) {
-	var self = this;
-	//Perform your installation tasks here
+    var self = this;
+    //Perform your installation tasks here
 };
 
 onkyoControl.prototype.getConf = function(varName) {
-	var self = this;
-	//Perform your installation tasks here
+    var self = this;
+    //Perform your installation tasks here
 };
 
 onkyoControl.prototype.setConf = function(varName, varValue) {
-	var self = this;
-	//Perform your installation tasks here
+    var self = this;
+    //Perform your installation tasks here
 };
