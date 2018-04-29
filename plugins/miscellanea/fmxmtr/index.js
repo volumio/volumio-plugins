a6c6ca2575ba46ca25ae141ff3653ee43fc72cd4
Randy Bancroft <randybancroft@verizon.net>
2018-04-29 13:58:52 +0000

    updating plugin fmxmtr version 1.0.1

--- plugins/miscellanea/fmxmtr/index.js
+++ plugins/miscellanea/fmxmtr/index.js
@@ -8,6 +8,9 @@ var execSync = require('child_process').execSync;
 
 var shell = require('shelljs');
 
+// Create ShellCommand variable.  It is used in several functions
+var ShellCommand = '';
+
 // Define variables to hold register values read from transmitter module
 var Reg0x00 = 0;
 var Reg0x01 = 0;
@@ -41,6 +44,7 @@ var PGAmpStored=0;
 var PltAdjStored=0;
 var PhTCnstStored=0;
 var PDPAStored=false;
+var StartUpStored=false;
 
 
 
@@ -64,20 +68,190 @@ Controllerfmxmtr.prototype.onVolumioStart = function()
 	this.config = new (require('v-conf'))();
 	this.config.loadFile(configFile);
 
-// Debug code - remove when done
-    // For debugging purposes
-    var keys=config.getKeys(configFile);
-    self.logger.info("Config file: " + configFile);
-    FreqStored=this.config.get('Freq');
-    BaseBoostStored=this.config.get('BaseBoost');
-    RFGainStored=this.config.get('RFGain');
-    MuteAudioStored=this.config.get('MuteAudio');
-    MonoAudioStored=this.config.get('MonoAudio');
-    PGAModStored=this.config.get('PGAMod');
-    PGAmpStored=this.config.get('PGAmp');
-    PltAdjStored=this.config.get('PltAdj');
-    PhTCnstStored=this.config.get('PhTCnst');
-    PDPAStored=this.config.get('PDPA');
+// check to see if config.json exists
+    var checkfile = ("/bin/cp " + configFile + " /tmp/config.txt");
+    var exists = false;
+    try {
+	var cp3 = execSync(checkfile);
+	exists = true;
+   } catch (err) {
+	self.logger.info('fmxmtr config.json does not exist');
+	exists = false;
+   }
+
+    if(exists){
+// Disable broadcasting.  a.k.a. Airplane mode
+    ShellCommand = 'i2cset -y ' + this.config.get('I2C_BUS') + ' 0x' + this.config.get('I2C_ADDRESS') + ' 0x0B 0x20';
+    shell.exec(ShellCommand);
+    self.logger.info('Disabled RF output');
+
+// Config.json exists, read user configuration from config.json and write to transmitter module
+// I2C_BUS
+        var I2CBusSave = this.config.get('I2C_BUS');
+// I2C_ADDRESS
+        var I2CAddressSave = this.config.get('I2C_ADDRESS');
+// Freq
+        var FreqSave = this.config.get('Freq');
+        FreqSave = (FreqSave * 1000)/50;
+// Convert config.json values to register values
+// Frequency is 12 bits divided over three registers
+// Register 0x02 bit 0x07 = Freq bit 0x01
+// Register 0x00 bits 0x07 to 0x00 = Freq bits 0x08 to 0x01
+// Register 0x01 bits 0x02 to 0x00 = Freq bits 0x11 to 0x09
+// Split Freq into three registers
+        var Reg0x02Freq = (FreqSave & 0x0001) *8;
+        var Reg0x00Freq = (FreqSave / 2) & 0x00ff;
+        var Reg0x01Freq = ((FreqSave & 0x0600) / 512);
+// BaseBoost
+        var BaseBoostSave = this.config.get('BaseBoost');
+// BaseBoost is two bits in one register
+// Register 0x04 bits 0x01 to 0x00 = BaseBoost bits 0x01 to 0x00
+        var Reg0x04BaseBoost = (BaseBoostSave & 0x03);
+// RFGain
+        var RFGainSave = this.config.get('RFGain');
+// RFGain is 4 bits divided over tthree registers
+// Register 0x02 bit 0x06 = RFGain bit 0x03
+// Register 0x13 bit 0x07 = RFGain bit 0x02
+// Register 0x01 bits 0x07 to 0x06 - RFGain bits 0x01 to 0x00
+        var Reg0x02FRGain = (RFGainSave & 0x08) * 8;
+        var Reg0x13FRGain = (RFGainSave & 0x04) * 32;
+        var Reg0x01FRGain = (RFGainSave & 0x03) * 64;
+// MuteAudio
+        var MuteAudioSave = this.config.get('MuteAudio');
+// MuteAudio is one bit in one register
+// Register 0x02 bit 0x03 = MuteAudio logic state
+        var Reg0x02MuteAudio = 0x00;
+        if (MuteAudioSave){
+            Reg0x02MuteAudio = 0x08;
+        } else {
+            Reg0x02MuteAudio = 0x00;
+        };
+// MonoAudio
+        var MonoAudioSave = this.config.get('MonoAudio');
+// MonoAudio is one bit in one register
+// Register 0x04 bit 0x06 = MonoAudio logic state
+        var Reg0x04MonoAudio = 0x00;
+        if (MonoAudioSave){
+            Reg0x04MonoAudio = 0x40;
+        } else {
+            Reg0x04MonoAudio = 0x00;
+        };
+// PGAMod
+        var PGAModSave = this.config.get('PGAMod');
+// PGAMod is three bits in one register
+// Register 0x10 bit 0x00 = PGAMod bit 0x00
+        var Reg0x10PGAMod = PGAModSave
+// PGAmp
+        var PGAmpSave = this.config.get('PGAmp');
+// PGAmp is 5 bits divided over two registers
+// Register 0x01 bits 0x05 to 0x03 = PGAmp bits 0x04 to 0x02 
+// Register 0x04 bits 0x05 to 0x04 - PGAmp bits 0x01 to 0x00
+        if (PGAmpSave < 0){
+                PGAmpSave = PGAmpSave * -1;
+            }
+            else{
+                PGAmpSave = (PGAmpSave *1) + 16;
+                };
+        var Reg0x01PGAmp = (PGAmpSave & 0x1c) * 2;
+        var Reg0x04PGAmp = (PGAmpSave & 0x03) * 16;
+// PltAdj
+        var PltAdjSave = this.config.get('PltAdj');
+// PltAdj is one bit in one register
+// Register 0x02 bit 0x02 = PltAdj bit 0x00
+        var Reg0x02PltAdj = (PltAdjSave & 0x01) * 4;
+// PhTCnst
+        var PhTCnstSave = this.config.get('PhTCnst');
+// PhTCnst is one bit in one register
+// Register 0x02 bit 0x00 = PhTCnst bit 0x00
+        var Reg0x02PhTCnst = (PhTCnstSave & 0x01);
+// PDPA
+        var PDPASave = this.config.get('PDPA');
+// PDPA is one bit in one register
+// Register 0x0B bit 0x05 = PDPA logic state
+        var Reg0x0BPDPA = 0x00;
+        if (PDPASave){
+            Reg0x0BPDPA = 0x20;
+        } else {
+            Reg0x0BPDPA = 0x00;
+        };
+// combine all bit per register
+// Register 0x00 is made up of Reg0x00Freq only
+        var Reg0x00Write = Reg0x00Freq;
+// Register 0x01 is made up of Reg0x01FRGain, Reg0x01PGAmp and Reg0x01Freq
+        var Reg0x01Write = (Reg0x01FRGain + Reg0x01PGAmp + Reg0x01Freq);
+//Register 0x02 is made up of Reg0x02Freq, Reg0x02FRGain, Reg0x02MuteAudio, Reg0x02PltAdj and Reg0x02PhTCnst
+        var Reg0x02Write = (Reg0x02 & 0x32) + (Reg0x02Freq + Reg0x02FRGain + Reg0x02MuteAudio + Reg0x02PltAdj + Reg0x02PhTCnst);
+// Register 0x04 is made up of Reg0x04MonoAudio, Reg0x04PGAmp and Reg0x04BaseBoost
+        var Reg0x04Write = (Reg0x04 & 0x8c) + (Reg0x04MonoAudio + Reg0x04PGAmp + Reg0x04BaseBoost);
+// Register 0x0b is made up of Reg0x0BPDPA only
+        var Reg0x0BWrite = (Reg0x0B & 0xdf) + Reg0x0BPDPA;
+// Register 0x10 is made up of Reg0x10PGAMod only
+        var Reg0x10Write = (Reg0x10 & 0xfe) + Reg0x10PGAMod;
+// Register 0x13 is made up of  only Reg0x13FRGain
+        var Reg0x13Write = (Reg0x13 & 0x7f) + Reg0x13FRGain;
+// Write registers
+        ShellCommand = 'i2cset -y ' + I2CBusSave + ' 0x' + I2CAddressSave + ' 0x00 0x' + Reg0x00Write.toString(16);
+        shell.exec(ShellCommand);
+        ShellCommand = 'i2cset -y ' + I2CBusSave + ' 0x' + I2CAddressSave + ' 0x01 0x' + Reg0x01Write.toString(16);
+        shell.exec(ShellCommand);
+        ShellCommand = 'i2cset -y ' + I2CBusSave + ' 0x' + I2CAddressSave + ' 0x02 0x' + Reg0x02Write.toString(16);
+        shell.exec(ShellCommand);
+        ShellCommand = 'i2cset -y ' + I2CBusSave + ' 0x' + I2CAddressSave + ' 0x04 0x' + Reg0x04Write.toString(16);
+        shell.exec(ShellCommand);
+        ShellCommand = 'i2cset -y ' + I2CBusSave + ' 0x' + I2CAddressSave + ' 0x0B 0x' + Reg0x0BWrite.toString(16);
+        shell.exec(ShellCommand);
+        ShellCommand = 'i2cset -y ' + I2CBusSave + ' 0x' + I2CAddressSave + ' 0x10 0x' + Reg0x10Write.toString(16);
+        shell.exec(ShellCommand);
+        ShellCommand = 'i2cset -y ' + I2CBusSave + ' 0x' + I2CAddressSave + ' 0x13 0x' + Reg0x13Write.toString(16);
+        shell.exec(ShellCommand);
+
+// Check to see if user desires to start transmitting on Volumio Start
+        if (this.config.get('StartUp')){
+            ShellCommand = 'i2cset -y ' + this.config.get('I2C_BUS') + ' 0x' + this.config.get('I2C_ADDRESS') + ' 0x0B 0x00';
+            shell.exec(ShellCommand);
+        }
+        else
+        {
+            ShellCommand = 'i2cset -y ' + this.config.get('I2C_BUS') + ' 0x' + this.config.get('I2C_ADDRESS') + ' 0x0B 0x20';
+            shell.exec(ShellCommand);
+        };
+    }
+    else {
+// config.json does not exist, create it, fill with defaults and write defaults to registers on transmitter module
+// Disable broadcasting using default values.  a.k.a. Airplane mode
+        shell.exec('i2cset -y 1 0x3e 0x0B 0x20');
+        self.logger.info('Disabled RF output');
+
+// Create file
+        ShellCommand = ('echo {} > ' + configFile);
+        shell.exec(ShellCommand);
+        self.logger.info('Created fmxmtr config.json');
+
+// Fill it with default values
+        self.config.set('I2C_BUS',1);
+        self.config.set('I2C_ADDRESS',"3e");
+        self.config.set('Freq',"94.5");
+        self.config.set('BaseBoost',0);
+        self.config.set('RFGain',15);
+        self.config.set('MuteAudio',false);
+        self.config.set('MonoAudio',false);
+        self.config.set('PGAMod',0);
+        self.config.set('PGAmp',0);
+        self.config.set('PltAdj',0);
+        self.config.set('PhTCnst',0);
+        self.config.set('PDPA',false);
+        self.config.set('StartUp',true);
+
+// Set default values on transmiter module
+    // Write default registers
+        shell.exec('i2cset -y 1 0x3e 0x00 0xb1');
+        shell.exec('i2cset -y 1 0x3e 0x01 0xe3');
+        shell.exec('i2cset -y 1 0x3e 0x02 0x40');
+        shell.exec('i2cset -y 1 0x3e 0x04 0x04');
+        shell.exec('i2cset -y 1 0x3e 0x0b 0x00');
+        shell.exec('i2cset -y 1 0x3e 0x10 0xa8');
+        shell.exec('i2cset -y 1 0x3e 0x13 0x80');
+    };
 
     return libQ.resolve();
 }
@@ -110,7 +284,7 @@ Controllerfmxmtr.prototype.onStart = function() {
     PDPARead = ((Reg0x0B & 0x20)/32);
 
 
-	// Once the Plugin has successfull started resolve the promise
+// Once the Plugin has successfull started resolve the promise
 	defer.resolve();
 
     return defer.promise;
@@ -120,7 +294,7 @@ Controllerfmxmtr.prototype.onStop = function() {
     var self = this;
     var defer=libQ.defer();
 
-    // Once the Plugin has successfull stopped resolve the promise
+// Once the Plugin has successfull stopped resolve the promise
     defer.resolve();
 
     return libQ.resolve();
@@ -128,7 +302,7 @@ Controllerfmxmtr.prototype.onStop = function() {
 
 Controllerfmxmtr.prototype.onRestart = function() {
     var self = this;
-    // Optional, use if you need it
+// Optional, use if you need it
 
 
 };
@@ -146,56 +320,43 @@ Controllerfmxmtr.prototype.getUIConfig = function() {
         __dirname+'/i18n/strings_en.json',
         __dirname + '/UIConfig.json')
         .then(function(uiconf)
-        {
-
-	uiconf.sections[0].content[0].value.value = self.config.get('I2C_BUS');
-	uiconf.sections[0].content[0].value.label = self.config.get('I2C_BUS');
-
-    uiconf.sections[0].content[1].value = self.config.get('I2C_ADDRESS');
-
-	uiconf.sections[0].content[2].config.bars[0].value = self.config.get('Freq');
-
-	uiconf.sections[0].content[3].value.value = self.config.get('BaseBoost');
-	uiconf.sections[0].content[3].value.label = self.config.get('BaseBoost');
-
-    uiconf.sections[0].content[4].value.value = self.config.get('RFGain');
-    uiconf.sections[0].content[4].value.label = self.config.get('RFGain');
-
-    uiconf.sections[0].content[5].value = self.config.get('MuteAudio');
-
-    uiconf.sections[0].content[6].value = self.config.get('MonoAudio');
-
-    uiconf.sections[0].content[7].value.value = self.config.get('PGAMod');
-    uiconf.sections[0].content[7].value.label = self.config.get('PGAMod');
-
-    if (self.config.get('PGAMod') == 0) {
-        uiconf.sections[0].content[8].config.bars[0].value = (self.config.get('PGAmp') - (self.config.get('PGAmp') % 5));
-        uiconf.sections[0].content[8].config.bars[0].step = 5;
-        } else {
-            uiconf.sections[0].content[8].config.bars[0].step = 1;
-        uiconf.sections[0].content[8].config.bars[0].value = self.config.get('PGAmp');
-        };
-
-    uiconf.sections[0].content[9].value.value = self.config.get('PltAdj');
-    uiconf.sections[0].content[9].value.label = self.config.get('PltAdj');
-
-    uiconf.sections[0].content[10].value.value = self.config.get('PhTCnst');
-    uiconf.sections[0].content[10].value.label = self.config.get('PhTCnst');
-
-    uiconf.sections[0].content[11].value = self.config.get('PDPA');
-        
-            defer.resolve(uiconf);
-
-
-
-            defer.resolve(uiconf);
-        })
-        .fail(function()
+            {
+                uiconf.sections[0].content[0].value.value = self.config.get('I2C_BUS');
+                uiconf.sections[0].content[0].value.label = self.config.get('I2C_BUS');
+                uiconf.sections[0].content[1].value = self.config.get('I2C_ADDRESS');
+                uiconf.sections[0].content[2].config.bars[0].value = self.config.get('Freq');
+                uiconf.sections[0].content[3].value.value = self.config.get('BaseBoost');
+                uiconf.sections[0].content[3].value.label = self.config.get('BaseBoost');
+                uiconf.sections[0].content[4].value.value = self.config.get('RFGain');
+                uiconf.sections[0].content[4].value.label = self.config.get('RFGain');
+                uiconf.sections[0].content[5].value = self.config.get('MuteAudio');
+                uiconf.sections[0].content[6].value = self.config.get('MonoAudio');
+                uiconf.sections[0].content[7].value.value = self.config.get('PGAMod');
+                uiconf.sections[0].content[7].value.label = self.config.get('PGAMod');
+                if (self.config.get('PGAMod') == 0) {
+                    uiconf.sections[0].content[8].config.bars[0].value = (self.config.get('PGAmp') - (self.config.get('PGAmp') % 5));
+                    uiconf.sections[0].content[8].config.bars[0].step = 5;
+                } else {
+                    uiconf.sections[0].content[8].config.bars[0].step = 1;
+                    uiconf.sections[0].content[8].config.bars[0].value = self.config.get('PGAmp');
+                };
+                uiconf.sections[0].content[9].value.value = self.config.get('PltAdj');
+                uiconf.sections[0].content[9].value.label = self.config.get('PltAdj');
+                uiconf.sections[0].content[10].value.value = self.config.get('PhTCnst');
+                uiconf.sections[0].content[10].value.label = self.config.get('PhTCnst');
+                uiconf.sections[0].content[11].value = self.config.get('PDPA');
+                uiconf.sections[0].content[12].value = self.config.get('StartUp');
+                    
+                defer.resolve(uiconf);
+            }
+        )
+    .fail(function()
         {
             defer.reject(new Error());
-        });
+        }
+    );
 
-    return defer.promise;
+return defer.promise;
 };
 
 
@@ -203,17 +364,16 @@ Controllerfmxmtr.prototype.setUIConfig = function(data) {
 	var self = this;
 	//Perform your installation tasks here
 
-
 };
 
 Controllerfmxmtr.prototype.getConf = function(varName) {
 	var self = this;
-	//Perform your installation tasks here
+//Perform your installation tasks here
 };
 
 Controllerfmxmtr.prototype.setConf = function(varName, varValue) {
 	var self = this;
-	//Perform your installation tasks here
+//Perform your installation tasks here
 };
 
 // define what happens when the user clicks the 'save' button on the settings page
@@ -267,8 +427,6 @@ Controllerfmxmtr.prototype.SaveFMXmtrOptions = function(data) {
     var Reg0x13FRGain = (RFGainSave & 0x04) * 32;
     var Reg0x01FRGain = (RFGainSave & 0x03) * 64;
 
-
-
 // Save MuteAudio
     var MuteAudioSave = data.MuteAudio;
     self.config.set('MuteAudio',MuteAudioSave);
@@ -276,12 +434,12 @@ Controllerfmxmtr.prototype.SaveFMXmtrOptions = function(data) {
 // Convert save values to register values
 // MuteAudio is one bit in one register
 // Register 0x02 bit 0x03 = MuteAudio logic state
-        var Reg0x02MuteAudio = 0x00;
-        if (MuteAudioSave){
-            Reg0x02MuteAudio = 0x08;
-        } else {
-            Reg0x02MuteAudio = 0x00;
-        };
+    var Reg0x02MuteAudio = 0x00;
+    if (MuteAudioSave){
+        Reg0x02MuteAudio = 0x08;
+    } else {
+        Reg0x02MuteAudio = 0x00;
+    };
 
 // Save MonoAudio
     var MonoAudioSave = data.MonoAudio;
@@ -290,12 +448,12 @@ Controllerfmxmtr.prototype.SaveFMXmtrOptions = function(data) {
 // Convert save values to register values
 // MonoAudio is one bit in one register
 // Register 0x04 bit 0x06 = MonoAudio logic state
-        var Reg0x04MonoAudio = 0x00;
-        if (MonoAudioSave){
-            Reg0x04MonoAudio = 0x40;
-        } else {
-            Reg0x04MonoAudio = 0x00;
-        };
+    var Reg0x04MonoAudio = 0x00;
+    if (MonoAudioSave){
+        Reg0x04MonoAudio = 0x40;
+    } else {
+        Reg0x04MonoAudio = 0x00;
+    };
 
 // Save PGAMod
     var PGAModSave = data.PGAMod.value;
@@ -339,66 +497,55 @@ Controllerfmxmtr.prototype.SaveFMXmtrOptions = function(data) {
 // Convert save values to register values
 // PhTCnst is one bit in one register
 // Register 0x02 bit 0x00 = PhTCnst bit 0x00
-     var Reg0x02PhTCnst = (PhTCnstSave & 0x01);
+    var Reg0x02PhTCnst = (PhTCnstSave & 0x01);
 
 // Save PDPA
     var PDPASave = data.PDPA;
     self.config.set('PDPA',PDPASave);
 
+// Save StartUp
+    var StartUpSave = data.StartUp;
+    self.config.set('StartUp',StartUpSave);
+
 // Convert save values to register values
 // PDPA is one bit in one register
 // Register 0x0B bit 0x05 = PDPA logic state
-        var Reg0x0BPDPA = 0x00;
-        if (PDPASave){
-            Reg0x0BPDPA = 0x20;
-        } else {
-            Reg0x0BPDPA = 0x00;
-        };
+    var Reg0x0BPDPA = 0x00;
+    if (PDPASave){
+        Reg0x0BPDPA = 0x20;
+    } else {
+        Reg0x0BPDPA = 0x00;
+    };
 
 // combine all bit per register
 // Register 0x00 is made up of Reg0x00Freq only
     var Reg0x00Write = Reg0x00Freq;
-
 // Register 0x01 is made up of Reg0x01FRGain, Reg0x01PGAmp and Reg0x01Freq
     var Reg0x01Write = (Reg0x01FRGain + Reg0x01PGAmp + Reg0x01Freq);
-
 //Register 0x02 is made up of Reg0x02Freq, Reg0x02FRGain, Reg0x02MuteAudio, Reg0x02PltAdj and Reg0x02PhTCnst
     var Reg0x02Write = (Reg0x02 & 0x32) + (Reg0x02Freq + Reg0x02FRGain + Reg0x02MuteAudio + Reg0x02PltAdj + Reg0x02PhTCnst);
-
 // Register 0x04 is made up of Reg0x04MonoAudio, Reg0x04PGAmp and Reg0x04BaseBoost
     var Reg0x04Write = (Reg0x04 & 0x8c) + (Reg0x04MonoAudio + Reg0x04PGAmp + Reg0x04BaseBoost);
-
 // Register 0x0b is made up of Reg0x0BPDPA only
     var Reg0x0BWrite = (Reg0x0B & 0xdf) + Reg0x0BPDPA;
-
 // Register 0x10 is made up of Reg0x10PGAMod only
     var Reg0x10Write = (Reg0x10 & 0xfe) + Reg0x10PGAMod;
-
 // Register 0x13 is made up of  only Reg0x13FRGain
     var Reg0x13Write = (Reg0x13 & 0x7f) + Reg0x13FRGain;
-
 // Write registers
     //shell.exec('i2cset -y 1 0x3e 0x00 0x00');
-    var ShellCommand = '';
     ShellCommand = 'i2cset -y ' + I2CBusSave + ' 0x' + I2CAddressSave + ' 0x00 0x' + Reg0x00Write.toString(16);
     shell.exec(ShellCommand);
-    
     ShellCommand = 'i2cset -y ' + I2CBusSave + ' 0x' + I2CAddressSave + ' 0x01 0x' + Reg0x01Write.toString(16);
     shell.exec(ShellCommand);
-    
     ShellCommand = 'i2cset -y ' + I2CBusSave + ' 0x' + I2CAddressSave + ' 0x02 0x' + Reg0x02Write.toString(16);
     shell.exec(ShellCommand);
-    
     ShellCommand = 'i2cset -y ' + I2CBusSave + ' 0x' + I2CAddressSave + ' 0x04 0x' + Reg0x04Write.toString(16);
     shell.exec(ShellCommand);
-    
     ShellCommand = 'i2cset -y ' + I2CBusSave + ' 0x' + I2CAddressSave + ' 0x0B 0x' + Reg0x0BWrite.toString(16);
     shell.exec(ShellCommand);
-
     ShellCommand = 'i2cset -y ' + I2CBusSave + ' 0x' + I2CAddressSave + ' 0x10 0x' + Reg0x10Write.toString(16);
     shell.exec(ShellCommand);
-
     ShellCommand = 'i2cset -y ' + I2CBusSave + ' 0x' + I2CAddressSave + ' 0x13 0x' + Reg0x13Write.toString(16);
     shell.exec(ShellCommand);
-
 };
--- plugins/miscellanea/fmxmtr/uninstall.sh
+++ plugins/miscellanea/fmxmtr/uninstall.sh
@@ -2,7 +2,6 @@
 
 # Uninstall dependendencies
 # apt-get remove -y
-rm -R /data/plugins/miscellanea/fmxmtr
-rm -R /data/configuration/miscellanea/fmxmtr
+
 echo "Done"
 echo "pluginuninstallend"
