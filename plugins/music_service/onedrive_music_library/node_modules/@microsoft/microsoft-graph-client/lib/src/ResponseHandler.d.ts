353c2ea710dc2a4459bddf0f464997ee28a2543c
ghembs <ggambacorta88@gmail.com>
2018-11-20 13:59:41 +0000

    cleaned modules

--- plugins/music_service/onedrive_music_library/node_modules/@microsoft/microsoft-graph-client/lib/src/ResponseHandler.d.ts
+++
@@ -1,5 +0,0 @@
-import { GraphRequestCallback, GraphError } from "./common";
-export declare class ResponseHandler {
-    static init(err: any, res: any, callback: GraphRequestCallback): void;
-    static ParseError(rawErr: any): GraphError;
-}
--- plugins/music_service/onedrive_music_library/node_modules/@microsoft/microsoft-graph-client/lib/src/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { Options } from "./common";
-import { GraphRequest } from "./GraphRequest";
-export declare class Client {
-    config: Options;
-    static init(clientOptions?: Options): Client;
-    api(path: string): GraphRequest;
-}
