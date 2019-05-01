34b491e8fba951b9c16baec325b1337fdb1012fe
nanomesher <info@nanomesher.com>
2019-05-01 11:03:34 +0800

    fix install script to install luma.core

--- plugins/miscellanea/nanosound/install.sh
+++ plugins/miscellanea/nanosound/install.sh
@@ -11,20 +11,20 @@ systemctl disable lirc.service
 systemctl stop lirc.service
 
 echo "Creating lircrc file"
-touch /etc/lirc/lircrc
+sudo touch /etc/lirc/lircrc
 #END OF LIRC
 
 #START OF python devs
 sudo apt-get -y install i2c-tools python-smbus python-pip python-dev python-imaging python-rpi.gpio python-lirc
 
 
-sudo -H pip install --upgrade setuptools
-sudo -H easy_install -U pip
-sudo -H pip install --upgrade python-mpd2 socketIO-client
+sudo pip install --upgrade setuptools
+sudo easy_install -U pip
+sudo pip install --upgrade python-mpd2 socketIO-client
 
 #extra for DAC2
 sudo apt-get -y install libfreetype6-dev libjpeg-dev build-essential
-sudo -H pip install --upgrade luma.oled
+sudo pip install --upgrade luma.oled
 
 #Install OLED service
 cd /tmp
