e27ec9c3fea020cfa5da680849475ed3ac472e87
nanomesher <info@nanomesher.com>
2019-05-01 12:34:36 +0800

    Improve uninstall script

--- plugins/music_service/nanosound_cd/uninstall.sh
+++ plugins/music_service/nanosound_cd/uninstall.sh
@@ -9,7 +9,11 @@ sudo /bin/systemctl stop nanosoundcd_progressweb
 sudo /bin/systemctl disable nanosoundcd_web
 sudo /bin/systemctl disable nanosoundcd_progressweb
 
-rm /home/volumio/nanomesher_nanosoundcd -r
+sudo rm /lib/systemd/system/nanosoundcd_progressweb.service
+sudo rm /lib/systemd/system/nanosoundcd_web.service
+sudo /bin/systemctl daemon-reload
+
+sudo rm -r /home/volumio/nanomesher_nanosoundcd
 
 echo "Done"
 echo "pluginuninstallend"
