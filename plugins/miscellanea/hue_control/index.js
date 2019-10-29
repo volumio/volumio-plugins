42ad43d029a7955bb7902f1cc0111833e24f5718
André <andre.iske@mailbox.org>
2019-10-29 22:09:38 +0100

    Reformat and change this to self in constructor

--- plugins/miscellanea/hue_control/index.js
+++ plugins/miscellanea/hue_control/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var libQ = require('kew');
-var fs=require('fs-extra');
+var fs = require('fs-extra');
 var config = new (require('v-conf'))();
 var exec = require('child_process').exec;
 var execSync = require('child_process').execSync;
@@ -23,12 +23,12 @@ const hueDeviceName = 'volumio';
 function hueControl(context) {
     var self = this;
 
-	this.context = context;
-	this.commandRouter = this.context.coreCommand;
-	this.logger = new HueLogger(this.context.logger);
-	this.configManager = this.context.configManager;
+    self.context = context;
+    self.commandRouter = this.context.coreCommand;
+    self.logger = new HueLogger(this.context.logger);
+    this.configManager = this.context.configManager;
 
-	this.volumioClient = null;
+    this.volumioClient = null;
     this.playerStatus = null;
 
     this.switchOffTimer = null;
@@ -40,70 +40,69 @@ function HueLogger(volumioLogger) {
     this.logger = volumioLogger;
 }
 
-HueLogger.prototype.info = function(message) {
+HueLogger.prototype.info = function (message) {
     var self = this;
     self.logger.info(`hue_control: ${message}`);
 };
 
-HueLogger.prototype.warn = function(message) {
+HueLogger.prototype.warn = function (message) {
     var self = this;
     self.logger.warn(`hue_control: ${message}`);
 };
 
-HueLogger.prototype.error = function(message) {
+HueLogger.prototype.error = function (message) {
     var self = this;
     self.logger.error(`hue_control: ${message}`);
 };
 
-HueLogger.prototype.debug = function(message) {
+HueLogger.prototype.debug = function (message) {
     var self = this;
     self.logger.debug(`hue_control: ${message}`);
 };
 
-hueControl.prototype.onVolumioStart = function()
-{
-	var self = this;
+hueControl.prototype.onVolumioStart = function () {
+    var self = this;
 
-	self.logger.debug('onVolumioStart');
-	
-	var configFile=this.commandRouter.pluginManager.getConfigurationFile(this.context,'config.json');
-	this.config = new (require('v-conf'))();
-	this.config.loadFile(configFile);
+    self.logger.debug('onVolumioStart');
+
+    var configFile = this.commandRouter.pluginManager.getConfigurationFile(this.context, 'config.json');
+    this.config = new (require('v-conf'))();
+    this.config.loadFile(configFile);
 
     return libQ.resolve();
 };
 
-hueControl.prototype.onStart = function() {
-	var self = this;
+hueControl.prototype.onStart = function () {
+    var self = this;
 
     self.logger.debug('onStart');
 
-	// connect to the volumio service and listen for player state changes
-	self.volumioClient = io.connect('http://localhost:3000');
-	self.volumioClient.on('pushState', self.onVolumioState.bind(self));
-	// request the value of the current player state
-	self.volumioClient.emit('getState', '');
-	
-	var defer=libQ.defer();
+    // connect to the volumio service and listen for player state changes
+    self.volumioClient = io.connect('http://localhost:3000');
+    self.volumioClient.on('pushState', self.onVolumioState.bind(self));
+    // request the value of the current player state
+    self.volumioClient.emit('getState', '');
+
+    var defer = libQ.defer();
 
 
-	// Once the Plugin has successfull started resolve the promise
-	defer.resolve();
+    // Once the Plugin has successfull started resolve the promise
+    defer.resolve();
 
     return defer.promise;
 };
 
-hueControl.prototype.onStop = function() {
-	var self = this;
+hueControl.prototype.onStop = function () {
+    var self = this;
 
-	self.logger.debug('onStop');
-	
-	if (self.volumioClient) {
+    self.logger.debug('onStop');
+
+    if (self.volumioClient) {
         self.volumioClient.disconnect();
         self.volumioClient = null;
     }
 
-    var defer=libQ.defer();
+    var defer = libQ.defer();
 
     // Once the Plugin has successfull stopped resolve the promise
     defer.resolve();
@@ -111,13 +110,13 @@ hueControl.prototype.onStop = function() {
     return libQ.resolve();
 };
 
-hueControl.prototype.onRestart = function() {
+hueControl.prototype.onRestart = function () {
     var self = this;
     // Optional, use if you need it
 };
 // UI Actions -----------------------------------------------------------------------------
 
-hueControl.prototype.pairBridge = function(data) {
+hueControl.prototype.pairBridge = function (data) {
     var self = this;
     self.logger.debug('pairBridge');
     if (!data['hue_bridge_address'] || 0 === data['hue_bridge_address'].length) {
@@ -179,7 +178,7 @@ async function createUser(ipAddress) {
 
 // Volumio Connection -----------------------------------------------------------------------------
 
-hueControl.prototype.onVolumioState = function(state) {
+hueControl.prototype.onVolumioState = function (state) {
     var self = this;
 
     if (state.status !== self.playerStatus) {
@@ -194,8 +193,8 @@ hueControl.prototype.onVolumioState = function(state) {
                 clearTimeout(self.switchOffTimer);
                 self.switchOffTimer = null;
             }
-			// switch on volumio device if available
-			// TODO: Replace with hue function
+            // switch on volumio device if available
+            // TODO: Replace with hue function
             // if (self.tradfriDeviceOrGroup) {
             //     // tradfri device available
             //     self.logger.info(`switch on ${self.tradfriDeviceOrGroup.name}`);
@@ -215,14 +214,14 @@ hueControl.prototype.onVolumioState = function(state) {
     }
 };
 
-hueControl.prototype.switchOffTimeout = function() {
+hueControl.prototype.switchOffTimeout = function () {
     var self = this;
 
     // timeout expired
     self.switchOffTimer = null;
 
-	// switch off volumio device if available
-	// TODO: Replace with hue function
+    // switch off volumio device if available
+    // TODO: Replace with hue function
     // if (self.tradfriDeviceOrGroup) {
     //     // tradfri device available
     //     self.logger.info(`switch off ${self.tradfriDeviceOrGroup.name}`);
@@ -232,62 +231,60 @@ hueControl.prototype.switchOffTimeout = function() {
 
 // Configuration Methods -----------------------------------------------------------------------------
 
-hueControl.prototype.getUIConfig = function() {
+hueControl.prototype.getUIConfig = function () {
     console.log("getUIConfig")
     var defer = libQ.defer();
     var self = this;
 
-	var lang_code = this.commandRouter.sharedVars.get('language_code');
-	
-    self.commandRouter.i18nJson(__dirname+'/i18n/strings_'+lang_code+'.json',
-        __dirname+'/i18n/strings_en.json',
-        __dirname + '/UIConfig.json')
-        .then(function(uiconf)
-        {
-			
-			// Bridge Address
-			uiconf.sections[0].content[0].value = self.config.get('hue_bridge_address');
-			uiconf.sections[1].content[1].value = self.config.get('hue_bridge_address');
+    var lang_code = this.commandRouter.sharedVars.get('language_code');
 
-			// switch off delay
-			uiconf.sections[2].content[0].value = self.config.get('switch_off_delay');
+    self.commandRouter.i18nJson(__dirname + '/i18n/strings_' + lang_code + '.json',
+        __dirname + '/i18n/strings_en.json',
+        __dirname + '/UIConfig.json')
+        .then(function (uiconf) {
+
+            // Bridge Address
+            uiconf.sections[0].content[0].value = self.config.get('hue_bridge_address');
+            uiconf.sections[1].content[1].value = self.config.get('hue_bridge_address');
+
+            // switch off delay
+            uiconf.sections[2].content[0].value = self.config.get('switch_off_delay');
 
             // remove either the connect on or disconnect section
-			var indexOfSectionToRemove = (isConnected()) ? 0 : 1;
-			uiconf.sections.splice(indexOfSectionToRemove, 1);
+            var indexOfSectionToRemove = (isConnected()) ? 0 : 1;
+            uiconf.sections.splice(indexOfSectionToRemove, 1);
 
-			// Remove device selection if bridge is not connected
-			if(!isConnected()) {
-				uiconf.sections[1].content.splice(1,1);
-			}
+            // Remove device selection if bridge is not connected
+            if (!isConnected()) {
+                uiconf.sections[1].content.splice(1, 1);
+            }
 
             defer.resolve(uiconf);
         })
-        .fail(function()
-        {
+        .fail(function () {
             defer.reject(new Error());
         });
 
     return defer.promise;
 };
 
-hueControl.prototype.getConfigurationFiles = function() {
-	return ['config.json'];
+hueControl.prototype.getConfigurationFiles = function () {
+    return ['config.json'];
 };
 
-hueControl.prototype.setUIConfig = function(data) {
-	var self = this;
-	//Perform your installation tasks here
+hueControl.prototype.setUIConfig = function (data) {
+    var self = this;
+    //Perform your installation tasks here
 };
 
-hueControl.prototype.getConf = function(varName) {
-	var self = this;
-	//Perform your installation tasks here
+hueControl.prototype.getConf = function (varName) {
+    var self = this;
+    //Perform your installation tasks here
 };
 
-hueControl.prototype.setConf = function(varName, varValue) {
-	var self = this;
-	//Perform your installation tasks here
+hueControl.prototype.setConf = function (varName, varValue) {
+    var self = this;
+    //Perform your installation tasks here
 };
 
 
