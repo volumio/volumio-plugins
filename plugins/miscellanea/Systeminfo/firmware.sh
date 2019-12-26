f7c44f89c7d92c63af5d50adaad2d31c405a5b01
balbuze <balbuze@gmail.com>
2019-12-26 20:45:09 +0100

    systeminfo - firmware version for RPI

--- plugins/miscellanea/Systeminfo/firmware.sh
+++ plugins/miscellanea/Systeminfo/firmware.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-raw_firmware=`/opt/vc/bin/vcgencmd version`
-date_version=`echo $raw_firmware | awk '{print $1,$2,$3,$4}'`
-git_version=`echo $raw_firmware | awk '{print $10}'`
+baseresult=`/opt/vc/bin/vcgencmd version`
+date_v=`echo $baseresult | awk '{print $1,$2,$3,$4}'`
+git_v=`echo $baseresult | awk '{print $10}'`
 
 echo "{"
-echo "\"firmware\" : {\"type\":\"string\",\"value\":\"$date_version - $git_version\"}"
+echo "\"firmware\" : {\"type\":\"string\",\"value\":\"$date_v - $git_v\"}"
 echo "}"
 exit 0
