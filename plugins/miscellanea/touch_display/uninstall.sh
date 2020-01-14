694ec44dd1208dadb953a6976217f0a8566ef843
gvolt <36240972+gvolt@users.noreply.github.com>
2020-01-14 09:04:14 +0100

    Added missing space character

--- plugins/miscellanea/touch_display/uninstall.sh
+++ plugins/miscellanea/touch_display/uninstall.sh
@@ -8,7 +8,7 @@ sudo rm /lib/systemd/system/volumio-kiosk.service
 if [ -f /etc/udev/rules.d/99-backlight.rules ]; then
   sudo rm /etc/udev/rules.d/99-backlight.rules
 fi
-if [ -f /etc/X11/xorg.conf.d/95-touch_display-plugin.conf]; then
+if [ -f /etc/X11/xorg.conf.d/95-touch_display-plugin.conf ]; then
   sudo rm /etc/X11/xorg.conf.d/95-touch_display-plugin.conf
 fi
 
