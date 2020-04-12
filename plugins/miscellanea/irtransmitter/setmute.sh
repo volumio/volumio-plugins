68c8a13455d488955a082b2ae8f7e1783e622c88
Jochen <jochen.arlt@gmail.com>
2020-04-12 11:47:47 +0000

    Mute script returns volume value resulting from mute operation

--- plugins/miscellanea/irtransmitter/setmute.sh
+++ plugins/miscellanea/irtransmitter/setmute.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 if [ -z "$1" ]; then 
-    echo usage: $0  1 to mute, 0 to unmute
+    echo Send mute signal to IR transmitter and return current volume value.
+    echo usage: $0 1  to mute / $0 0 to unmute
     exit
 fi
 FNMUTE="/data/plugins/miscellanea/irtransmitter/mute"
 MUTE=$(cat "$FNMUTE")
 if [ $1 -eq $MUTE ]; then
-  echo "No mute change"
+#  echo "No mute change"
 else 
-  echo "Toggle mute"
+#  echo "Toggle mute"
   irsend SEND_ONCE CamAudioOne KEY_MUTE
   echo $1 > "$FNMUTE"
 fi
-echo $1
+if [ $1 -eq "1" ]; then
+  echo 0
+else
+  echo `cat /data/plugins/miscellanea/irtransmitter/currentvolume`
+fi
