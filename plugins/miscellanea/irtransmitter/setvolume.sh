e5715502297485fef920cb58b531e2cc5c2eb086
Jochen <jochen.arlt@gmail.com>
2020-04-13 16:02:31 +0000

    Set volume script now sets the requested volume rather than just changing by one step

--- plugins/miscellanea/irtransmitter/setvolume.sh
+++ plugins/miscellanea/irtransmitter/setvolume.sh
@@ -3,22 +3,34 @@ if [ -z "$1" ]; then
     echo usage: $0 volume
     exit
 fi
+# Name of the file stroring current volume:
 FNVOL="/data/plugins/miscellanea/irtransmitter/currentvolume"
+# Read value:
 PREV=$(cat "$FNVOL")
 # echo $PREV, $1
+CMDSTRING=""
 if [ $1 -eq $PREV ]; then
   echo "No volume change"
   exit
 elif [ $1 -gt $PREV ]; then
-  echo "Volume increased"
-  irsend SEND_ONCE CamAudioOne KEY_VOLUMEUP
-  PREV=$(($PREV+1))
+  CMDSTRING="KEY_VOLUMEUP"
+  REP=$(($1 - $PREV))
+  echo "Volume increased by $REP steps from $PREV to $1"
 else
-  echo "Volume decreased"
-  irsend SEND_ONCE CamAudioOne KEY_VOLUMEDOWN
-  PREV=$(($PREV-1))
+  CMDSTRING="KEY_VOLUMEDOWN"
+  REP=$(($PREV - $1))
+  echo "Volume decreased by $REP steps from $PREV to $1"
 fi
-# Currently the volume is not set to the number passed to the routine but just de-/increased
-# by one. Should really loop ir commands...
-echo $PREV
-echo $PREV > "$FNVOL"
+#echo $REP
+# Build command list for irsend
+COUNT=1
+COMMAND=""
+while [ $COUNT -le $REP ]
+do
+  COUNT=$(($COUNT + 1))
+  COMMAND="$COMMAND $CMDSTRING"
+done
+
+# Send actual command
+irsend SEND_ONCE CamAudioOne$COMMAND
+echo $1 > "$FNVOL"
