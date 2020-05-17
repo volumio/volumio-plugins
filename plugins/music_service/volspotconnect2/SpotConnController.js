0ce5cb9831dc3e24cadcd5460c9164aa7920e633
ashthespy <ashthespy@gmail.com>
2020-05-17 20:44:05 +0200

    Sync to ashthespy/Volumio-SpotifyConnect

--- plugins/music_service/volspotconnect2/SpotConnController.js
+++ plugins/music_service/volspotconnect2/SpotConnController.js
@@ -14,6 +14,26 @@ class SpotConnEvents extends EventEmitter {
     this._udpsource.on('message', msg => {
       this.parseData(msg.toString());
     });
+    this.Events = {
+      // Daemon state events
+      PlaybackActive: Symbol('PlaybackActive'),
+      PlaybackInactive: Symbol('PlaybackInactive'),
+      DeviceActive: Symbol('DeviceActive'),
+      DeviceInactive: Symbol('DeviceInactive'),
+      SinkActive: Symbol('SinkActive'),
+      SinkInactive: Symbol('SinkInactive'),
+      // PLayback events
+      Metadata: Symbol('Metadata'),
+      Token: Symbol('Token'),
+      Seek: Symbol('Seek'),
+      Volume: Symbol('Volume'),
+      Status: Symbol('Status'),
+      // Misc
+      Pong: Symbol('Pong'),
+      Unknown: Symbol('Unknown'),
+      // Responses
+      PongPause: Symbol('PongPause')
+    };
   }
 
   parseData (msg) {
@@ -21,51 +41,61 @@ class SpotConnEvents extends EventEmitter {
       const data = JSON.parse(msg);
       switch (true) {
         case 'metadata' in data:
-          this.emit('metadata', data.metadata);
+          this.emit(this.Events.Metadata, data.metadata);
           break;
         case 'token' in data:
-          this.emit('token', data.token);
+          this.emit(this.Events.Token, data.token);
           break;
         case 'position_ms' in data:
-          this.emit('seek', data.position_ms);
+          this.emit(this.Events.Seek, data.position_ms);
           break;
         case 'volume' in data:
-          this.emit('volume', data.volume);
+          this.emit(this.Events.Volume, data.volume);
           break;
         case 'state' in data:
-          this.emit('status', data.state.status);
+          this.emit(this.Events.Status, data.state.status);
           break;
+        case 'pong' in data:
+          this.emit(this.Events.Pong, data.pong); // General
+          switch (data.pong) {
+            case 'Pause':
+              this.emit(this.Events.PongPause);
+              break;
+          }
+          break;
+
         default:
-          if (data) this.emit('unknown', data);
+          if (data) this.emit(this.Events.Unknown, data);
       }
     } catch (e) {
       switch (msg) {
-        case 'kSpPlaybackNotifyBecameActive':
-          this.emit('SessionActive', '');
+        case 'kSpPlaybackActive':
+          this.emit(this.Events.PlaybackActive);
+          break;
+
+        case 'kSpPlaybackInactive':
+          this.emit(this.Events.PlaybackInactive);
           break;
 
         case 'kSpDeviceActive':
-          this.emit('DeviceActive', '');
-          break;
-
-        case 'kSpSinkActive':
-          this.emit('SinkActive', '');
+          this.emit(this.Events.DeviceActive);
           break;
 
         case 'kSpDeviceInactive':
-          this.emit('DeviceInactive', '');
+          this.emit(this.Events.DeviceInactive);
+          break;
+
+        case 'kSpSinkActive':
+          this.emit(this.Events.SinkActive);
           break;
 
         case 'kSpSinkInactive':
-          this.emit('SinkInactive', '');
-          break;
-
-        case 'kSpPlaybackNotifyBecameInactive':
-          this.emit('SessionInactive');
+          this.emit(this.Events.SinkInactive);
           break;
 
         default:
-          this.emit('unknown', msg);
+          // TODO Strip whitespace:
+          if (msg) this.emit(this.Events.Unknown, msg, e);
       }
     }
   }
@@ -87,10 +117,15 @@ class SpotConnEvents extends EventEmitter {
 const msgMap = new Map();
 
 // values:
-msgMap.set('HELLO', [0x1]);
-msgMap.set('HEARTBEAT', [0x2]);
-msgMap.set('GET_TOKEN', [0x3]);
-msgMap.set('STOP', [0x4]);
+msgMap.set('Hello', [0x1]);
+msgMap.set('HeartBeat', [0x2]);
+msgMap.set('ReqToken', [0x3]);
+msgMap.set('Pause', [0x4]);
+msgMap.set('Play', [0x5]);
+msgMap.set('PlayPause', [0x6]);
+msgMap.set('Next', [0x7]);
+msgMap.set('Prev', [0x8]);
+msgMap.set('Volume', [0x9]);
 
 module.exports = {
   SpotConnEvents: SpotConnEvents,
