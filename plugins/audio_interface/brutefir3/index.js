5760c0b7b17dfe15a761b107c0dad4d174f88b37
balbuze <balbuze@gmail.com>
2020-08-22 14:51:49 +0200

    brutefir 3 - work on modular_alsa

--- plugins/audio_interface/brutefir3/index.js
+++ plugins/audio_interface/brutefir3/index.js
@@ -73,10 +73,9 @@ ControllerBrutefir.prototype.onStart = function () {
 
   socket.emit('getState', '');
   socket.emit('pause');
-  // self.sendvolumelevel();
   self.config.set('displayednameofset', "Set used is 1");
   self.config.set('setUsedOfFilters', "1");
-  //self.askForRebootFirstUse()
+  self.askForRebootFirstUse()
   self.autoconfig()
 
     .then(function (e) {
@@ -108,7 +107,7 @@ ControllerBrutefir.prototype.onStop = function () {
     socket.off();
   });
   //self.commandRouter.executeOnPlugin('audio_interface', 'alsa_controller', 'updateALSAConfigFile')
-  
+
 
   defer.resolve();
   return libQ.resolve();
@@ -737,7 +736,47 @@ ControllerBrutefir.prototype.getAdditionalConf = function (type, controller, dat
 };
 
 // Plugin methods -----------------------------------------------------------------------------
- 
+
+//------------Ask for reboot for first time 
+ControllerBrutefir.prototype.askForRebootFirstUse = function () {
+  const self = this;
+
+  if (self.config.get('askForReboot')) {
+   // self.saveHardwareAudioParameters();
+    var responseData = {
+      title: self.commandRouter.getI18nString('FIRST_USE'),
+      message: self.commandRouter.getI18nString('FIRST_USE_MESS'),
+      size: 'lg',
+      buttons: [
+        {
+          name: self.commandRouter.getI18nString('CONTINUE'),
+          class: 'btn btn-cancel',
+          emit: '',
+          payload: ''
+        },
+        {
+          name: 'Reboot',
+          class: 'btn btn-info',
+          emit: 'callMethod',
+          payload: { 'endpoint': 'audio_interface/brutefir', 'method': 'setFalseReboot', 'data': '' }
+        }
+      ]
+    }
+    self.commandRouter.broadcastMessage("openModal", responseData);
+  }
+};
+
+ControllerBrutefir.prototype.setFalseReboot = function () {
+  const self = this;
+  self.config.set('askForReboot', false);
+  setTimeout(function () {
+    console.log(self.config.get('askForReboot'));
+    socket.emit('reboot');
+  }, 500);
+};
+
+// 
+
 
 //here we load snd_aloop module to provide a Loopback device
 ControllerBrutefir.prototype.modprobeLoopBackDevice = function () {
