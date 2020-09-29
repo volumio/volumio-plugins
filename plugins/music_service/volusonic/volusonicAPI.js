95aa944bb17ac3f8c5af88ad9c35f498236fc5ed
foobarjo <jo@foobarjo.org>
2020-09-29 19:05:12 +0200

    salt+md5 switch

--- plugins/music_service/volusonic/volusonicAPI.js
+++ plugins/music_service/volusonic/volusonicAPI.js
@@ -7,70 +7,82 @@ var unirest = require('unirest');
 
 module.exports = volusonicApi;
 
-function volusonicApi(log,config) {
+function volusonicApi(log, config) {
 	var self = this;
-    	var logger = log;
-	var cache = cachemanager.caching({ store: 'memory', max: 50000, ttl: config.get('timeOut')});
+	var logger = log;
+	var cache = cachemanager.caching({
+		store: 'memory',
+		max: 50000,
+		ttl: config.get('timeOut')
+	});
 
-	var cacheGet = function (key) {
+	var cacheGet = function(key) {
 		var self = this;
 		return cache.get(key);
 	};
 
-	var cacheSet = function (key, value) {
+	var cacheSet = function(key, value) {
 		var self = this;
 		cache.set(key, value);
 	};
 
-	var cacheRemove = function () {
+	var cacheRemove = function() {};
+
+	var cacheReset = function() {
+		var self = this;
+		cache = cachemanager.caching({
+			store: 'memory',
+			max: 50000,
+			ttl: config.get('timeOut')
+		});
 	};
 
-	var cacheReset = function () {
+	var get = function(command, id, params) {
 		var self = this;
-		cache = cachemanager.caching({ store: 'memory', max: 50000, ttl: config.get('timeOut')});
+		var defer = libQ.defer();
+
+		var cached = cacheGet(command + id)
+			.then(function(cached) {
+				if (cached == undefined) {
+					cached = submitQuery(command + '?' + params)
+						.then(function(cached) {
+							if (cached['subsonic-response'].status != 'failed') {
+								cacheSet(command + id, cached);
+							}
+							defer.resolve(cached);
+						});
+				} else {
+					defer.resolve(cached);
+				}
+			});
+		return defer.promise;
 	};
 
-	var get = function (command, id, params) {
+	var getAuth = function(user, pass, salt) {
 		var self = this;
-                var defer = libQ.defer();
+		var auth;
 
-                var cached = cacheGet( command + id )
-                .then(function(cached){
-                        if ( cached == undefined){
-                                cached = submitQuery(command + '?' + params)
-                                .then(function(cached){
-                                        if (cached['subsonic-response'].status!='failed'){
-                                                cacheSet(command + id, cached);
-                                        }
-                                        defer.resolve(cached);
-                                });
-                        }
-                        else {
-                                defer.resolve(cached);
-                        }
-                });
-                return defer.promise;
-	};
+		if (salt) {
+			var makesalt = function() {
+				var salt = "";
+				var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-	var getAuth = function (user, pass) {
-		var self = this;
+				for (var i = 0; i < 5; i++)
+					salt += possible.charAt(Math.floor(Math.random() * possible.length));
+				return salt;
+			}
 
-		var makesalt = function() {
-  			var salt = "";
-  			var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-  			for (var i = 0; i < 5; i++)
-    				salt += possible.charAt(Math.floor(Math.random() * possible.length));
-  			return salt;
+			var salt = makesalt();
+			var token = crypto.createHash('md5').update(pass + salt).digest("hex");
+			var auth = "u=" + user + "&t=" + token + "&s=" + salt + "&v=1.13.0&f=json&c=volusonic";
+		} else {
+			var hex = Buffer.from(pass, 'utf8');
+			auth = "u=" + user + "&p=enc:" + hex.toString('hex') + "&v=1.13.0&f=json&c=volusonic";
 		}
-
-		var salt = makesalt();
-		var token = crypto.createHash('md5').update(pass + salt).digest("hex");
-		var auth = "u=" + user + "&t=" + token + "&s=" + salt + "&v=1.13.0&f=json&c=volusonic";
 		return auth;
 	};
 
-	var submitQuery = function (query) {
+	var submitQuery = function(query) {
 		var self = this;
 		var defer = libQ.defer();
 
@@ -78,14 +90,13 @@ function volusonicApi(log,config) {
 		unirest
 			.get(uri)
 			.strictSSL(false)
-            		.end(function (response) {
-                		if (response.ok) {
-                    			defer.resolve(response.body);
-                		}
-                		else {
-                    			defer.reject(new Error('submitQuery'));
-                		}
-            		});
+			.end(function(response) {
+				if (response.ok) {
+					defer.resolve(response.body);
+				} else {
+					defer.reject(new Error('submitQuery'));
+				}
+			});
 		return defer.promise;
 
 	};
