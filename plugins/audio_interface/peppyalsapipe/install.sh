0fa50e960d8b099dab24c7e51e585bc6af566d6e
balbuze <balbuze@gmail.com>
2020-12-30 11:37:31 +0100

    peppyalsapipe - 0.0.3

--- plugins/audio_interface/peppyalsapipe/install.sh
+++ plugins/audio_interface/peppyalsapipe/install.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
-#pipepath=/data/plugins/audio_interface/peppyalsapipe/myfifopeppy
-#peppymeterpath=/data/plugins/audio_interface/peppyalsapipe/peppymeter
+peppymeterpath=/data/plugins/audio_interface/peppyalsapipe/peppymeter
 
 echo "Installing peppyalsa plugin dependencies"
-sudo cp /data/plugins/audio_interface/peppyalsapipe/libpeppyalsa.so /usr/local/lib/
-#/usr/bin/mkfifo $pipepath
-#sudo chmod 777 $pipepath
-#sudo chown volumio $pipepath
-#sudo chgrp volumio $pipepath
+#sudo cp /data/plugins/audio_interface/peppyalsapipe/libpeppyalsa.so /usr/local/lib/
 
 echo "cloning peppymeter repo"
-#git clone https://github.com/project-owner/PeppyMeter.git $peppymeterpath
+git clone https://github.com/balbuze/PeppyMeter $peppymeterpath
 
-echo "installing pyton3-pygame"
-#sudo apt-get install python3-pygame
+echo "installing apt packages"
+sudo apt-get install python3-pygame
+sudo apt-get install build-essential autoconf automake libtool libasound2-dev libfftw3-dev
+
+echo "Installing peppyalsa plugin"
+mkdir /tmp/peppyalsa
+git clone https://github.com/project-owner/peppyalsa.git /tmp/peppyalsa
+cd /tmp/peppyalsa
+aclocal && libtoolize
+autoconf && automake --add-missing
+./configure && make
+sudo make install
+
 
 #required to end the plugin install
 echo "plugininstallend"
