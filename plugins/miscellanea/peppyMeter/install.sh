28e9dbc56eeb1bc363c64ce74c1897deef0cad79
balbuze <balbuze@gmail.com>
2021-03-13 16:56:39 +0100

    peppymete - service was not installed

--- plugins/miscellanea/peppyMeter/install.sh
+++ plugins/miscellanea/peppyMeter/install.sh
@@ -29,8 +29,8 @@ if [ ! -f "/usr/local/lib/libpeppyalsa.so" ];
 		echo "Detected cpu architecture as $cpu"
 		if [ $cpu = "armv7l" ] || [ $cpu = "aarch64" ] || [ $cpu = "armv6l" ]
 		then
-		sudo mv $LIB/arm/libpeppyalsa.so /usr/local/lib/
-
+		sudo mv $LIB/arm/libpeppyalsa.so.0.0 /usr/local/lib/
+        sudo ln -s -f libpeppyalsa.so.0.0.0 libpeppyalsa.so
 		else
 			echo "Sorry, cpu is $cpu and your device is not yet supported !"
 			echo "exit now..."
