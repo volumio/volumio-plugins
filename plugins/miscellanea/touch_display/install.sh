5d8f388afdf4fb9867db4a95a8250d539ea42e15
gvolt <jem_7@web.de>
2021-04-05 16:05:45 +0200

    Touch Display plugin: Add scaling option

--- plugins/miscellanea/touch_display/install.sh
+++ plugins/miscellanea/touch_display/install.sh
@@ -77,10 +77,14 @@ sudo echo "#!/bin/bash
 while true; do timeout 3 bash -c \"</dev/tcp/127.0.0.1/3000\" >/dev/null 2>&1 && break; done
 sed -i 's/\"exited_cleanly\":false/\"exited_cleanly\":true/' /data/volumiokiosk/Default/Preferences
 sed -i 's/\"exit_type\":\"Crashed\"/\"exit_type\":\"None\"/' /data/volumiokiosk/Default/Preferences
+if [ -L /data/volumiokiosk/SingletonCookie ]; then
+  rm -rf /data/volumiokiosk/Singleton*
+fi
 openbox-session &
 while true; do
   /usr/bin/chromium-browser \\
     --simulate-outdated-no-au='Tue, 31 Dec 2099 23:59:59 GMT' \\
+    --force-device-scale-factor=1 \\
     --disable-pinch \\
     --kiosk \\
     --no-first-run \\
