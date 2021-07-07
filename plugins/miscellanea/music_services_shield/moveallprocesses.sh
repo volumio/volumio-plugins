00dcfc12aa2fccdc552266f6137f8049eb2ea27f
Pete Callaghan <wpete.callaghan@gmail.com>
2021-07-07 14:25:42 +0000

    Apply configuration to scripts that build the shield. Default process priority to off.

--- plugins/miscellanea/music_services_shield/moveallprocesses.sh
+++ plugins/miscellanea/music_services_shield/moveallprocesses.sh
@@ -3,15 +3,45 @@
 FULL_PATH_TO_SCRIPT="$(realpath "$0")"
 HERE="$(dirname "$FULL_PATH_TO_SCRIPT")"
 CPUSPARAMETERFILE="${HERE}/config/userCpuSpec.config"
+USERMPDCONFIGFILE="${HERE}/config/userMpd.config"
+USERSPOTIFYCONFIGFILE="${HERE}/config/userSpotify.config"
+RTMPDCONFIGFILE="${HERE}/config/rtMpd.config"
+RTSPOTIFYCONFIGFILE="${HERE}/config/rtSpotify.config"
+
 CPUCONFIG=$(cat $CPUSPARAMETERFILE)
+USERMPDCONFIG=$(cat $USERMPDCONFIGFILE)
+USERSPOTIFYCONFIG=$(cat $USERSPOTIFYCONFIGFILE)
+RTMPDCONFIG=$(cat $RTMPDCONFIGFILE)
+RTSPOTIFYCONFIG=$(cat $RTSPOTIFYCONFIGFILE)
+
+if [ $USERMPDCONFIG = "true" ] 
+then
+MPDGROUP="user"
+else
+MPDGROUP="system"
+fi
+
+if [ $USERSPOTIFYCONFIG = "true" ]
+then
+SPOTIFYGROUP="user"
+else
+SPOTIFYGROUP="system"
+fi
 
 cset shield -c $CPUCONFIG > /dev/null
 
-/data/plugins/miscellanea/music_services_shield/moveprocess.sh mpd > /dev/null
-/data/plugins/miscellanea/music_services_shield/moveprocess.sh vollibrespot  > /dev/null
+/data/plugins/miscellanea/music_services_shield/moveprocess.sh mpd $MPDGROUP > /dev/null
+/data/plugins/miscellanea/music_services_shield/moveprocess.sh vollibrespot $SPOTIFYGROUP > /dev/null
 cset shield
 
+if [ $RTMPDCONFIG = "true" ]
+then
 /data/plugins/miscellanea/music_services_shield/setrtpriority.sh mpd
+fi
+
+if [ $RTSPOTIFYCONFIG = "true" ]
+then
 /data/plugins/miscellanea/music_services_shield/setrtpriority.sh vollibrespot
+fi
 
 
--- plugins/miscellanea/music_services_shield/setrtpriority.sh
+++ plugins/miscellanea/music_services_shield/setrtpriority.sh
@@ -1,6 +1,13 @@
+#!/bin/sh
+#Define the path
+FULL_PATH_TO_SCRIPT="$(realpath "$0")"
+HERE="$(dirname "$FULL_PATH_TO_SCRIPT")"
+PARAMETERFILE="${HERE}/config/rtPriority.config"
+CONFIG=$(cat $PARAMETERFILE)
+
 pids=$(pgrep -x $1)
 for pid in $pids
 do
-  chrt -r -p 99 $pid
+  chrt -r -p $CONFIG $pid
 done
 
