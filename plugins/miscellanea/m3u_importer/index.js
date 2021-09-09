9cbf6296a3dacd2653bc8fca8a039a76cad16c4f
volumio <skip@gfrn.org>
2021-09-09 15:08:52 +0000

    WIP - Ui working, no actual conversion code yet.

--- plugins/miscellanea/m3u_importer/index.js
+++ plugins/miscellanea/m3u_importer/index.js
@@ -9,12 +9,12 @@ var execSync = require('child_process').execSync;
 
 module.exports = m3uImporter;
 function m3uImporter(context) {
-	var self = this;
+    var self = this;
 
-	this.context = context;
-	this.commandRouter = this.context.coreCommand;
-	this.logger = this.context.logger;
-	this.configManager = this.context.configManager;
+    this.context = context;
+    this.commandRouter = this.context.coreCommand;
+    this.logger = this.context.logger;
+    this.configManager = this.context.configManager;
 
 }
 
@@ -22,21 +22,22 @@ function m3uImporter(context) {
 
 m3uImporter.prototype.onVolumioStart = function()
 {
-	var self = this;
-	var configFile=this.commandRouter.pluginManager.getConfigurationFile(this.context,'config.json');
-	this.config = new (require('v-conf'))();
-	this.config.loadFile(configFile);
+    var self = this;
+    var configFile=this.commandRouter.pluginManager.getConfigurationFile(this.context,'config.json');
+    this.config = new (require('v-conf'))();
+    this.config.loadFile(configFile);
 
     return libQ.resolve();
 }
 
 m3uImporter.prototype.onStart = function() {
     var self = this;
-	var defer=libQ.defer();
+    self.commandRouter.pushToastMessage('info', 'I was called!', 'Please wait');
+    var defer=libQ.defer();
 
 
-	// Once the Plugin has successfull started resolve the promise
-	defer.resolve();
+    // Once the Plugin has successfull started resolve the promise
+    defer.resolve();
 
     return defer.promise;
 };
@@ -83,185 +84,37 @@ m3uImporter.prototype.getUIConfig = function() {
 };
 
 m3uImporter.prototype.getConfigurationFiles = function() {
-	return ['config.json'];
+    return ['config.json'];
 }
 
 m3uImporter.prototype.setUIConfig = function(data) {
-	var self = this;
-	//Perform your installation tasks here
+    var self = this;
+    //Perform your installation tasks here
 };
 
 m3uImporter.prototype.getConf = function(varName) {
-	var self = this;
-	//Perform your installation tasks here
+    var self = this;
+    //Perform your installation tasks here
 };
 
 m3uImporter.prototype.setConf = function(varName, varValue) {
-	var self = this;
-	//Perform your installation tasks here
+    var self = this;
+    //Perform your installation tasks here
 };
 
 
-
-// Playback Controls ---------------------------------------------------------------------------------------
-// If your plugin is not a music_sevice don't use this part and delete it
-
-
-m3uImporter.prototype.addToBrowseSources = function () {
-
-	// Use this function to add your music service plugin to music sources
-    //var data = {name: 'Spotify', uri: 'spotify',plugin_type:'music_service',plugin_name:'spop'};
-    this.commandRouter.volumioAddToBrowseSources(data);
-};
-
-m3uImporter.prototype.handleBrowseUri = function (curUri) {
+m3uImporter.prototype.doImport = function(data) {
     var self = this;
 
-    //self.commandRouter.logger.info(curUri);
-    var response;
+    self.commandRouter.logger.info('m3uImporter.doImport called 1');
 
+	self.commandRouter.logger.info('  m3u_dir: ' + data['m3u_dir'])
+	self.commandRouter.logger.info('  which: ' + data['which'].value)
 
-    return response;
-};
-
-
-
-// Define a method to clear, add, and play an array of tracks
-m3uImporter.prototype.clearAddPlayTrack = function(track) {
-	var self = this;
-	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'm3uImporter::clearAddPlayTrack');
-
-	self.commandRouter.logger.info(JSON.stringify(track));
-
-	return self.sendSpopCommand('uplay', [track.uri]);
-};
-
-m3uImporter.prototype.seek = function (timepos) {
-    this.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'm3uImporter::seek to ' + timepos);
-
-    return this.sendSpopCommand('seek '+timepos, []);
-};
-
-// Stop
-m3uImporter.prototype.stop = function() {
-	var self = this;
-	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'm3uImporter::stop');
-
+    if (data['overwrite_existing']) {
+        overwrite = "yes";
+    }
+    // self.commandRouter.pushToastMessage('info', 'I was called 3', overwrite, data['m3u_dir']);
 
 };
 
-// Spop pause
-m3uImporter.prototype.pause = function() {
-	var self = this;
-	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'm3uImporter::pause');
-
-
-};
-
-// Get state
-m3uImporter.prototype.getState = function() {
-	var self = this;
-	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'm3uImporter::getState');
-
-
-};
-
-//Parse state
-m3uImporter.prototype.parseState = function(sState) {
-	var self = this;
-	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'm3uImporter::parseState');
-
-	//Use this method to parse the state and eventually send it with the following function
-};
-
-// Announce updated State
-m3uImporter.prototype.pushState = function(state) {
-	var self = this;
-	self.commandRouter.pushConsoleMessage('[' + Date.now() + '] ' + 'm3uImporter::pushState');
-
-	return self.commandRouter.servicePushState(state, self.servicename);
-};
-
-
-m3uImporter.prototype.explodeUri = function(uri) {
-	var self = this;
-	var defer=libQ.defer();
-
-	// Mandatory: retrieve all info for a given URI
-
-	return defer.promise;
-};
-
-m3uImporter.prototype.getAlbumArt = function (data, path) {
-
-	var artist, album;
-
-	if (data != undefined && data.path != undefined) {
-		path = data.path;
-	}
-
-	var web;
-
-	if (data != undefined && data.artist != undefined) {
-		artist = data.artist;
-		if (data.album != undefined)
-			album = data.album;
-		else album = data.artist;
-
-		web = '?web=' + nodetools.urlEncode(artist) + '/' + nodetools.urlEncode(album) + '/large'
-	}
-
-	var url = '/albumart';
-
-	if (web != undefined)
-		url = url + web;
-
-	if (web != undefined && path != undefined)
-		url = url + '&';
-	else if (path != undefined)
-		url = url + '?';
-
-	if (path != undefined)
-		url = url + 'path=' + nodetools.urlEncode(path);
-
-	return url;
-};
-
-
-
-
-
-m3uImporter.prototype.search = function (query) {
-	var self=this;
-	var defer=libQ.defer();
-
-	// Mandatory, search. You can divide the search in sections using following functions
-
-	return defer.promise;
-};
-
-m3uImporter.prototype._searchArtists = function (results) {
-
-};
-
-m3uImporter.prototype._searchAlbums = function (results) {
-
-};
-
-m3uImporter.prototype._searchPlaylists = function (results) {
-
-
-};
-
-m3uImporter.prototype._searchTracks = function (results) {
-
-};
-
-m3uImporter.prototype.goto=function(data){
-    var self=this
-    var defer=libQ.defer()
-
-// Handle go to artist and go to album function
-
-     return defer.promise;
-};
